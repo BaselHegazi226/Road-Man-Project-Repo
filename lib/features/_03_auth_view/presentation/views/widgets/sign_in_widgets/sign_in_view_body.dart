42320b40e884c29cc4b0ee30052b9202814c1239
baselhegazi <baselmohammed226@gmail.com>
2025-02-28 14:15:31 +0200

    add Assets.kLoginImage to AuthImageSection parameter image (  AuthImageSection(image: Assets.kLoginImage),)

--- lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_view_body.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/common_widgets/auth_image_section.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_component_section.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_image_section.dart';
+
+import '../../../../../../core/helper/Assets.dart';
 
 class SignInViewBody extends StatelessWidget {
   const SignInViewBody({super.key});
@@ -9,7 +11,10 @@ class SignInViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
-      children: [SignInImageSection(), SignInComponentsSection()],
+      children: [
+        AuthImageSection(image: Assets.kLoginImage),
+        SignInComponentsSection(),
+      ],
     );
   }
 }
