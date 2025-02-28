2537c15ea13905765858313e61969ed72cb96e20
baselhegazi <baselmohammed226@gmail.com>
2025-02-28 14:09:04 +0200

    1- create sign_up_view.dart and 2- add sign_up_view_body.dart for his body

---
+++ lib/features/_03_auth_view/presentation/views/sign_up_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_up_widgets/sign_up_view_body.dart';
+
+import '../../../../core/helper/const_variables.dart';
+
+class SignUpView extends StatelessWidget {
+  const SignUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: kAppPrimaryColor, body: SignUpViewBody());
+  }
+}
