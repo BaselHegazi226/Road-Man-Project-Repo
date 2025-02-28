1eaff0b7ff6ff93ff10c1db99fc03da00a109bc8
baselhegazi <baselmohammed226@gmail.com>
2025-02-28 14:03:43 +0200

    add id for sign in view

--- lib/features/_03_auth_view/presentation/views/sign_in_view.dart
+++ lib/features/_03_auth_view/presentation/views/sign_in_view.dart
@@ -3,6 +3,7 @@ import 'package:road_man_project/core/helper/const_variables.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_view_body.dart';
 
 class SignInView extends StatelessWidget {
+  static String id = '/SignInView';
   const SignInView({super.key});
 
   @override
