9d3ef61e82c0ea34633d3a577993a6d672587d68
baselhegazi <baselmohammed226@gmail.com>
2025-02-28 14:10:38 +0200

    add id for sign_up_view.dart

--- lib/features/_03_auth_view/presentation/views/sign_up_view.dart
+++ lib/features/_03_auth_view/presentation/views/sign_up_view.dart
@@ -4,6 +4,7 @@ import 'package:road_man_project/features/_03_auth_view/presentation/views/widge
 import '../../../../core/helper/const_variables.dart';
 
 class SignUpView extends StatelessWidget {
+  static String id = '/SignUpView';
   const SignUpView({super.key});
 
   @override
