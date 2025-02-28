d82e114d521cba31fc8924a4ba6adc2cebabd392
baselhegazi <baselmohammed226@gmail.com>
2025-02-28 14:14:31 +0200

    1- rename widget to  auth_image_section.dart instead of sign_in_image_section.dart refactor code . 2- refactor code with accept image to make the widget is reusable.

--- lib/features/_03_auth_view/presentation/views/widgets/common_widgets/auth_image_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/common_widgets/auth_image_section.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import '../../../../../../core/helper/Assets.dart';
-
-class SignInImageSection extends StatelessWidget {
-  const SignInImageSection({super.key});
-
+class AuthImageSection extends StatelessWidget {
+  const AuthImageSection({super.key, required this.image});
+  final String image;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 64),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(Assets.kLoginImage, width: 170, height: 184),
-        ],
+        children: [SvgPicture.asset(image, width: 170, height: 184)],
       ),
     );
   }
