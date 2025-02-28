c69422616f4d00ce42c31f7585668cff07bfa127
baselhegazi <baselmohammed226@gmail.com>
2025-02-28 14:59:22 +0200

    create auth_component_section.dart instead of specify it to sign in only

---
+++ lib/features/_03_auth_view/presentation/views/widgets/common_widgets/auth_component_section.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../core/helper/const_variables.dart';
+import 'auth_title.dart';
+
+class AuthComponentSection extends StatelessWidget {
+  const AuthComponentSection({
+    super.key,
+    required this.title,
+    required this.child,
+  });
+  final String title;
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: kSecondlyDarkWhiteColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(32),
+            topRight: Radius.circular(32),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: AuthTitle(title: title),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: kSecondlyLightWhiteColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(32),
+                    topRight: Radius.circular(32),
+                  ),
+                ),
+                child: child,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_component_section_body.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_component_section_body.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../../../../../../core/helper/const_variables.dart';
+import '../common_widgets/auth_component_section.dart';
 import '../common_widgets/auth_sub_title.dart';
-import '../common_widgets/auth_title.dart';
 import '../common_widgets/social_auth_card_section.dart';
 import 'input_data_section.dart';
 
@@ -11,45 +10,21 @@ class SignInComponentSectionBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          child: AuthTitle(title: 'Login to your account'),
-        ),
-        Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              color: kSecondlyLightWhiteColor,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(32),
-                topRight: Radius.circular(32),
-              ),
-            ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 24,
-                    bottom: 16,
-                    left: 16,
-                    right: 16,
-                  ),
-                  child: const SocialAuthCardSection(),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                  child: const AuthSubTitle(
-                    subTitle: 'Or use your email account',
-                  ),
-                ),
-                InputDataSection(),
-              ],
-            ),
+    return AuthComponentSection(
+      title: 'Create your account',
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 24, bottom: 16, left: 16, right: 16),
+            child: const SocialAuthCardSection(),
           ),
-        ),
-      ],
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+            child: const AuthSubTitle(subTitle: 'Or use your email account'),
+          ),
+          InputDataSection(),
+        ],
+      ),
     );
   }
 }
