3dfb395e512a6256e7bae2c59c53300c6a0d3dd0
baselhegazi <baselmohammed226@gmail.com>
2025-02-28 14:20:45 +0200

    1- add GoRoute for sign_in_view 2- add GoRoute for sign_up_view

--- lib/core/utilities/app_router.dart
+++ lib/core/utilities/app_router.dart
@@ -1,43 +1,20 @@
 import 'package:go_router/go_router.dart';
 
 import '../../features/_03_auth_view/presentation/views/sign_in_view.dart';
+import '../../features/_03_auth_view/presentation/views/sign_up_view.dart';
 
 abstract class AppRouter {
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SignInView()),
-      //   GoRoute(
-      //     path: AuthView.id,
-      //     builder: (context, state) => const AuthView(),
-      //   ),
-      //   GoRoute(
-      //     path: SignInView.id,
-      //     builder: (context, state) => const SignInView(),
-      //   ),
-      //   GoRoute(
-      //     path: SignUpView.id,
-      //     builder: (context, state) => const SignUpView(),
-      //   ),
-      //   GoRoute(
-      //     path: ForgetPasswordView.id,
-      //     builder: (context, state) => const ForgetPasswordView(),
-      //   ),
-      //   GoRoute(
-      //     path: HomeView.id,
-      //     builder: (context, state) => const HomeView(),
-      //   ),
-      //   GoRoute(
-      //     path: VerifyEmailView.id,
-      //     builder: (context, state) => const VerifyEmailView(),
-      //   ),
-      //   GoRoute(
-      //     path: SignInPhoneNumView.id,
-      //     builder: (context, state) => const SignInPhoneNumView(),
-      //   ),
-      //   GoRoute(
-      //     path: OtpView.id,
-      //     builder: (context, state) => const OtpView(),
-      //   ),
+      GoRoute(
+        path: SignInView.id,
+        builder: (context, state) => const SignInView(),
+      ),
+      GoRoute(
+        path: SignUpView.id,
+        builder: (context, state) => const SignUpView(),
+      ),
     ],
   );
 }
