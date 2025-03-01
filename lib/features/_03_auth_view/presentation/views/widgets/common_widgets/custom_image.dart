cbc36526b75594003f80f7e18397cc49b728ce2e
baselhegazi <baselmohammed226@gmail.com>
2025-03-01 17:36:43 +0200

    add optional imageBottomPadding

--- lib/features/_03_auth_view/presentation/views/widgets/common_widgets/custom_image.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/common_widgets/custom_image.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class CustomImage extends StatelessWidget {
-  const CustomImage({super.key, required this.image});
+  const CustomImage({
+    super.key,
+    required this.image,
+    this.imageBottomPadding = 58,
+  });
   final String image;
+  final double imageBottomPadding;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(top: 48, bottom: 58),
+      padding: EdgeInsets.only(top: 48, bottom: imageBottomPadding),
       child: SvgPicture.asset(image, height: 350, width: 350),
     );
   }
