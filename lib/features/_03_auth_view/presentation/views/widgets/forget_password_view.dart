33144c75a369546a51bb85432818502b7c9d51ad
baselhegazi <baselmohammed226@gmail.com>
2025-03-01 15:07:57 +0200

    add custom_app_bar to ForgetPasswordView appbar

--- lib/features/_03_auth_view/presentation/views/widgets/forget_password_view.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/forget_password_view.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
 
+import '../../../../../core/helper/const_functions.dart';
+
 class ForgetPasswordView extends StatelessWidget {
   const ForgetPasswordView({super.key});
   static String id = '/ForgetPasswordView';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: kAppPrimaryWhiteColor);
+    return Scaffold(
+      backgroundColor: kAppPrimaryWhiteColor,
+      appBar: ConstFunctions.customAppBar(text: 'Forget Password'),
+    );
   }
 }
