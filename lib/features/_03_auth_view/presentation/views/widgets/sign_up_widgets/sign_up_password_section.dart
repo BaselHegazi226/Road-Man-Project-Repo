a880a44c1f17506ffca8828119c3f3e86493e20a
baselhegazi <baselmohammed226@gmail.com>
2025-03-01 14:37:14 +0200

    1- remove 'Confirm password' field 2- add validator to password field 3- add onChanged to password field 4- add CheckPasswordSection

--- lib/features/_03_auth_view/presentation/views/widgets/sign_up_widgets/sign_up_password_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_up_widgets/sign_up_password_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_up_widgets/check_password_section.dart';
 
 import '../../../../../../core/utilities/custom_text_form_field.dart';
 
@@ -29,20 +30,35 @@ class _SignUpPasswordSectionState extends State<SignUpPasswordSection> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      spacing: 8,
+      spacing: 16,
       children: [
         CustomTextFormField(
           hintText: 'Password',
           textEditingController: TextEditingController(),
           prefixIcon: Icons.lock_outline,
           suffixIcon: CupertinoIcons.eye_slash,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return 'Password is required';
+            } else if (!RegExp(
+              r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[A-Za-z\d\W]{8,}$',
+            ).hasMatch(value)) {
+              return 'Please enter a valid password! (Aa#12345)';
+            }
+            return null;
+          },
+          onChanged: (password) {
+            setState(() {
+              regExpForCheckPasswordVarsFun(password);
+            });
+          },
         ),
-
-        CustomTextFormField(
-          hintText: 'Confirm password',
-          textEditingController: TextEditingController(),
-          prefixIcon: Icons.lock_outline,
-          suffixIcon: CupertinoIcons.eye_slash,
+        CheckPasswordSection(
+          containsPassLength: containsPassLength,
+          containsSpecialChar: containsSpecialChar,
+          containsNumber: containsNumbers,
+          containsUpperCase: containsUpperCase,
+          containsLowerCase: containsLowerCase,
         ),
       ],
     );
