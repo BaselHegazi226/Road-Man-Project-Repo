82c5850ca44bf66da4c31507abcf8cb767e8fd81
baselhegazi <baselmohammed226@gmail.com>
2025-03-01 14:53:03 +0200

    create forget_password_view.dart

---
+++ lib/features/_03_auth_view/presentation/views/widgets/forget_password_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/core/helper/const_variables.dart';
+
+class ForgetPasswordView extends StatelessWidget {
+  const ForgetPasswordView({super.key});
+  static String id = '/ForgetPasswordView';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: kAppPrimaryWhiteColor);
+  }
+}
