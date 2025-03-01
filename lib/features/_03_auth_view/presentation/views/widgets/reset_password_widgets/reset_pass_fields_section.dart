bcb170a3fe57305e2fe25a902db99d9e6270d1f2
baselhegazi <baselmohammed226@gmail.com>
2025-03-01 17:33:13 +0200

    create reset_pass_fields_section.dart for control in reset view fields

---
+++ lib/features/_03_auth_view/presentation/views/widgets/reset_password_widgets/reset_pass_fields_section.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../../core/utilities/custom_text_form_field.dart';
+
+class ResetPassFieldsSection extends StatelessWidget {
+  const ResetPassFieldsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 16,
+      children: [
+        CustomTextFormField(
+          hintText: 'New Password',
+          prefixIcon: Icons.lock_outline,
+          suffixIcon: CupertinoIcons.eye_slash,
+          textEditingController: TextEditingController(),
+        ),
+
+        CustomTextFormField(
+          hintText: 'Confirm Password',
+          prefixIcon: Icons.lock_outline,
+          suffixIcon: CupertinoIcons.eye_slash,
+          textEditingController: TextEditingController(),
+        ),
+      ],
+    );
+  }
+}
