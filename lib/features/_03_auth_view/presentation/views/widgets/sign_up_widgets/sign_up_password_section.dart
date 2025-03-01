1a4c36457c1caab5bf28a9c53a51548efd734940
baselhegazi <baselmohammed226@gmail.com>
2025-03-01 13:44:15 +0200

    1- convert it to stateful widget 2- transfer variables and regExpForCheckPasswordVarsFun from sign_up_text_form_field_section.dart to sign_up_password_section.dart

--- lib/features/_03_auth_view/presentation/views/widgets/sign_up_widgets/sign_up_password_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_up_widgets/sign_up_password_section.dart
@@ -3,12 +3,33 @@ import 'package:flutter/material.dart';
 
 import '../../../../../../core/utilities/custom_text_form_field.dart';
 
-class SignUpPasswordSection extends StatelessWidget {
+class SignUpPasswordSection extends StatefulWidget {
   const SignUpPasswordSection({super.key});
+
+  @override
+  State<SignUpPasswordSection> createState() => _SignUpPasswordSectionState();
+}
+
+class _SignUpPasswordSectionState extends State<SignUpPasswordSection> {
+  bool obscurePassword = true;
+  bool containsUpperCase = false;
+  bool containsLowerCase = false;
+  bool containsSpecialChar = false;
+  bool containsNumbers = false;
+  bool containsPassLength = false;
+
+  void regExpForCheckPasswordVarsFun(String value) {
+    containsLowerCase = RegExp(r'(?=.*[a-z])').hasMatch(value);
+    containsUpperCase = RegExp(r'(?=.*[A-Z])').hasMatch(value);
+    containsNumbers = RegExp(r'(?=.*\d)').hasMatch(value);
+    containsSpecialChar = RegExp(r'[^a-zA-Z0-9\s]').hasMatch(value);
+    containsPassLength = RegExp(r'.{8,}').hasMatch(value);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      spacing: 8,
       children: [
         CustomTextFormField(
           hintText: 'Password',
