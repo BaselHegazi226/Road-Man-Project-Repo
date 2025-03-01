abb722e0cae58157067ae1a2808c1e201d6c70d4
baselhegazi <baselmohammed226@gmail.com>
2025-03-01 17:11:30 +0200

    add : 1- kForgetPasswordImage 2- kResetPasswordImage

--- lib/core/helper/Assets.dart
+++ lib/core/helper/Assets.dart
@@ -1,2 +1,6 @@
 const String kLoginImage = 'assets/images/auth/login_image.svg';
 const String kSignUpImage = 'assets/images/auth/sign_up_image.svg';
+const String kForgetPasswordImage =
+    'assets/images/auth/forget_password_image.svg';
+const String kResetPasswordImage =
+    'assets/images/auth/reset_password_image.svg';
