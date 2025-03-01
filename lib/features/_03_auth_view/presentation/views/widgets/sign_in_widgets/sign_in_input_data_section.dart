5528a69a4bd51eac519774d350299c0a45d65e34
baselhegazi <baselmohammed226@gmail.com>
2025-03-01 07:40:53 +0200

    add required parameters to OtherRegisterSection

---
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_input_data_section.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/sign_up_view.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_remember_password_section.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_text_form_fields_section.dart';
+
+import 'other_register_section.dart';
+import 'sign_in_button_section.dart';
+
+class SignInInputDataSection extends StatelessWidget {
+  const SignInInputDataSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        spacing: 16,
+        children: [
+          TextFormFieldsSection(),
+          RememberMeForgetPasswordSection(),
+          ButtonSection(),
+          Padding(
+            padding: EdgeInsets.only(top: 4),
+            child: OtherRegisterSection(
+              blackText: 'Don’t have an account? ',
+              blueText: 'Sign up',
+              onTap: () {
+                GoRouter.of(context).push(SignUpView.id);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
