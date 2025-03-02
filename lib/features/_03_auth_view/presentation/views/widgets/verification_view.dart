0133cf4762699e82a2b8969f3564263d4ca60dc3
baselhegazi <baselmohammed226@gmail.com>
2025-03-02 10:41:48 +0200

    put verification view body in verification_view in scaffold body

--- lib/features/_03_auth_view/presentation/views/widgets/verification_view.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/verification_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:road_man_project/core/helper/const_functions.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/verification_widgets/verification_view_body.dart';
 
 class VerificationView extends StatelessWidget {
   const VerificationView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: ConstFunctions.customAppBar(text: 'Verification'));
+    return Scaffold(
+      appBar: ConstFunctions.customAppBar(text: 'Verification'),
+      body: VerificationViewBody(),
+    );
   }
 }
