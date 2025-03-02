6be660bfa7a68e14f960c661c760d5130f272de9
baselhegazi <baselmohammed226@gmail.com>
2025-03-02 16:34:16 +0200

    add kCheckPasswordItemColor

--- lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_input_data_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_input_data_section.dart
@@ -2,41 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/sign_up_view.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/forget_password_view.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_button_section.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_remember_password_section.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_text_form_fields_section.dart';
 
 import 'other_register_section.dart';
-import 'sign_in_button_section.dart';
 
 class SignInInputDataSection extends StatelessWidget {
   const SignInInputDataSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        spacing: 16,
-        children: [
-          TextFormFieldsSection(),
-          RememberMeForgetPasswordSection(
-            forgetPasswordPressed: () {
-              GoRouter.of(context).push(ForgetPasswordView.id);
+    return Column(
+      spacing: 16,
+      children: [
+        TextFormFieldsSection(),
+        RememberMeForgetPasswordSection(
+          forgetPasswordPressed: () {
+            GoRouter.of(context).push(ForgetPasswordView.id);
+          },
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16),
+          child: SignInButtonSection(),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 4),
+          child: OtherRegisterSection(
+            blackText: 'Don’t have an account? ',
+            blueText: 'Sign up',
+            onTap: () {
+              GoRouter.of(context).push(SignUpView.id);
             },
           ),
-          ButtonSection(),
-          Padding(
-            padding: EdgeInsets.only(top: 4),
-            child: OtherRegisterSection(
-              blackText: 'Don’t have an account? ',
-              blueText: 'Sign up',
-              onTap: () {
-                GoRouter.of(context).push(SignUpView.id);
-              },
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
