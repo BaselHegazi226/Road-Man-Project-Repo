f5454a59d387aac5d6a3617631d5bf8ad112e442
baselhegazi <baselmohammed226@gmail.com>
2025-03-02 16:56:59 +0200

    import correct past for sign_in_component_section

--- lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_view_body.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/common_widgets/auth_image_section.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_component_section_body.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_component_section.dart';
 
 import '../../../../../../core/helper/Assets.dart';
 
