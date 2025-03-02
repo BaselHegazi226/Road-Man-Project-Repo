f911c81ab5b50682c0d3e846e900a9bb45f9a3eb
Mohamed Dwedar <dwedar4444@gmail.com>
2025-03-02 21:39:16 +0200

    - Remove id

--- lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_input_data_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_input_data_section.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/views/sign_up_view.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/forget_password_view.dart';
+import 'package:road_man_project/core/utilities/routes.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_button_section.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_remember_password_section.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_text_form_fields_section.dart';
-
 import 'other_register_section.dart';
 
 class SignInInputDataSection extends StatelessWidget {
@@ -19,7 +17,7 @@ class SignInInputDataSection extends StatelessWidget {
         TextFormFieldsSection(),
         RememberMeForgetPasswordSection(
           forgetPasswordPressed: () {
-            GoRouter.of(context).push(ForgetPasswordView.id);
+            GoRouter.of(context).push(Routes.forgetPasswordView);
           },
         ),
         Padding(
@@ -32,7 +30,7 @@ class SignInInputDataSection extends StatelessWidget {
             blackText: 'Don’t have an account? ',
             blueText: 'Sign up',
             onTap: () {
-              GoRouter.of(context).push(SignUpView.id);
+              GoRouter.of(context).push(Routes.signUpView);
             },
           ),
         ),
