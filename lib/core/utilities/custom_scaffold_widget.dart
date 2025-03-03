c8a50b3d2145c5e4e42b9319f6af6fe8522b0fee
baselhegazi <baselmohammed226@gmail.com>
2025-03-03 07:36:30 +0200

    make custom_scaffold_widget.dart for used in the same style in many views

---
+++ lib/core/utilities/custom_scaffold_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../helper/const_variables.dart';
+
+class CustomScaffoldWidget extends StatelessWidget {
+  const CustomScaffoldWidget({super.key, required this.body});
+  final Widget body;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: kAppPrimaryBlueColor,
+        resizeToAvoidBottomInset: true,
+        body: body,
+      ),
+    );
+  }
+}
