6c99a846434a198f42bf6004940ba9dc4af2daae
baselhegazi <baselmohammed226@gmail.com>
2025-03-09 17:27:54 +0200

    1- add custom auth title 2- add button to ResetPasswordInputDataSection

--- lib/features/_03_auth_view/presentation/views/widgets/reset_password_widgets/reset_password_view_body.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/reset_password_widgets/reset_password_view_body.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:road_man_project/core/helper/Assets.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/reset_password_widgets/reset_pass_fields_section.dart';
+import 'package:road_man_project/core/utilities/custom_flexible_widget.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/common_widgets/custom_auth_text.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/reset_password_widgets/reset_password_input_data_section.dart';
 
-import '../../../../../../core/helper/const_variables.dart';
-import '../../../../../../core/utilities/custom_text_button.dart';
-import '../../../../../../core/utilities/custom_title.dart';
 import '../common_widgets/custom_image.dart';
 
 class ResetPasswordViewBody extends StatelessWidget {
@@ -13,30 +12,20 @@ class ResetPasswordViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        spacing: 32,
-        children: [
-          CustomImage(imageBottomPadding: 21, image: kResetPasswordImage),
-          Text(
-            maxLines: 2,
-            'Create a new password for your account.                                                    Make sure to choose a strong and secure password',
-            style: TextStyle(
-              color: kTextPrimaryColor,
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              letterSpacing: -.228,
-              height: 1.5,
+    return CustomFlexibleWidget(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          spacing: 32,
+          children: [
+            CustomImage(imageBottomPadding: 21, image: kResetPasswordImage),
+            CustomAuthText(
+              text:
+                  'Create a new password for your account.                                                    Make sure to choose a strong and secure password',
             ),
-          ),
-          ResetPassFieldsSection(),
-          CustomTextButton(
-            onPressed: () {},
-            backgroundColor: kAppPrimaryBlueColor,
-            child: CustomTitle(title: 'Update Password'),
-          ),
-        ],
+            ResetPasswordInputDataSection(),
+          ],
+        ),
       ),
     );
   }
