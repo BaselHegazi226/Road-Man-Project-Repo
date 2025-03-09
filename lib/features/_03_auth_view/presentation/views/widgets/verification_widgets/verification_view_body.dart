f36d044da649032f11dbcbad387450094621a8b2
baselhegazi <baselmohammed226@gmail.com>
2025-03-09 17:34:53 +0200

    1- put custom image in clipRect to remove the remainder space below it 2-  add CustomAuthText 3- transform field and next into VerificationInputDataSection widget

--- lib/features/_03_auth_view/presentation/views/widgets/verification_widgets/verification_view_body.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/verification_widgets/verification_view_body.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:road_man_project/core/helper/const_variables.dart';
-import 'package:road_man_project/core/utilities/custom_text_button.dart';
-import 'package:road_man_project/core/utilities/custom_text_form_field_otp.dart';
-import 'package:road_man_project/core/utilities/custom_title.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/common_widgets/custom_auth_text.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/verification_widgets/verification_input_data_section.dart';
+import 'package:road_man_project/generated/assets.dart';
+
+import '../common_widgets/custom_image.dart';
 
 class VerificationViewBody extends StatelessWidget {
   const VerificationViewBody({super.key});
@@ -14,44 +15,22 @@ class VerificationViewBody extends StatelessWidget {
       child: Column(
         spacing: 24,
         children: [
+          ClipRect(
+            child: Align(
+              alignment: Alignment.topCenter,
+              heightFactor: .7,
+              child: CustomImage(image: Assets.authVerificationImage),
+            ),
+          ),
           Padding(
-            padding: EdgeInsets.only(top: 48, bottom: 8),
-            child: Text(
+            padding: EdgeInsets.only(top: 58),
+            child: CustomAuthText(
+              text:
+                  'We have sent a verification code to your email or phone number.             Enter the code below to complete the process',
               textAlign: TextAlign.center,
-              'We have sent a verification code to your email or phone number.             Enter the code below to complete the process',
-              style: TextStyle(
-                color: kTextPrimaryColor,
-                fontWeight: FontWeight.w400,
-                fontSize: 12,
-                height: 1.5,
-                letterSpacing: -.228,
-              ),
             ),
           ),
-          CustomTextFormFieldOtpPinPut(
-            validator: (value) {
-              return '';
-            },
-          ),
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              'Send again ?',
-              style: TextStyle(
-                color: kAppPrimaryBlueColor,
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 8),
-            child: CustomTextButton(
-              onPressed: () {},
-              backgroundColor: kAppPrimaryBlueColor,
-              child: CustomTitle(title: 'Verify'),
-            ),
-          ),
+          VerificationInputDataSection(),
         ],
       ),
     );
