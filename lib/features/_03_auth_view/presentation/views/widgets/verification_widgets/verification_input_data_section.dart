e66c3741e31041472ad2719b8b37424f430ad631
baselhegazi <baselmohammed226@gmail.com>
2025-03-10 08:45:44 +0200

    1- add autoValidateMode 2- add ( Padding(                    padding: EdgeInsets.only(bottom: 12),                    child: VerificationOtpFields(                      formKey: _formKey,                      autoValidateMode: autoValidateMode,                    ),                  ),) instead of (CustomTextFormFieldOtpPinPut(                                            validator: (value) {                                              if (value == null || value.isEmpty) {                                                return 'Please enter the OTP';                                              } else if (value.length < 4) {                                                return 'OTP must be 4 digits';                                              }                                              return null;                                            },))                                         3- remove textButton body and add this body to custom button

--- lib/features/_03_auth_view/presentation/views/widgets/verification_widgets/verification_input_data_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/verification_widgets/verification_input_data_section.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/verification_widgets/verification_otp_fields.dart';
 
 import '../../../../../../core/helper/const_variables.dart';
 import '../../../../../../core/utilities/custom_text_button.dart';
-import '../../../../../../core/utilities/custom_text_form_field_otp.dart';
 import '../../../../../../core/utilities/custom_title.dart';
 
 class VerificationInputDataSection extends StatefulWidget {
@@ -16,25 +16,21 @@ class VerificationInputDataSection extends StatefulWidget {
 class _VerificationInputDataSectionState
     extends State<VerificationInputDataSection> {
   final GlobalKey<FormState> _formKey = GlobalKey();
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
   @override
   Widget build(BuildContext context) {
     return Column(
       spacing: 24,
       children: [
-        CustomTextFormFieldOtpPinPut(
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Please enter the OTP';
-            } else if (value.length < 4) {
-              return 'OTP must be 4 digits';
-            }
-            return null;
-          },
+        Padding(
+          padding: EdgeInsets.only(bottom: 12),
+          child: VerificationOtpFields(
+            formKey: _formKey,
+            autoValidateMode: autoValidateMode,
+          ),
         ),
         TextButton(
-          onPressed: () {
-            if (!_formKey.currentState!.validate()) {}
-          },
+          onPressed: () {},
           child: Text(
             'Send again ?',
             style: TextStyle(
@@ -47,7 +43,18 @@ class _VerificationInputDataSectionState
         Padding(
           padding: EdgeInsets.only(top: 8),
           child: CustomTextButton(
-            onPressed: () {},
+            onPressed: () {
+              if (!_formKey.currentState!.validate()) {
+                setState(() {
+                  print("don't enter otp");
+                });
+              } else {
+                setState(() {
+                  autoValidateMode = AutovalidateMode.always;
+                  print('accept');
+                });
+              }
+            },
             backgroundColor: kAppPrimaryBlueColor,
             child: CustomTitle(title: 'Verify'),
           ),
