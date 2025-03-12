2952a88a01d0fa1a2ce1884a1bfe00b112032086
baselhegazi <baselmohammed226@gmail.com>
2025-03-12 10:50:27 +0200

    add profileUserImage to kProfileImage after i add it to assets to images to profile

--- lib/core/helper/Assets.dart
+++ lib/core/helper/Assets.dart
@@ -4,3 +4,4 @@ const String kForgetPasswordImage =
     'assets/images/auth/forget_password_image.svg';
 const String kResetPasswordImage =
     'assets/images/auth/reset_password_image.svg';
+const String kProfileImage = 'assets/images/profile/profile_user_image.png';
