ac729feb8c7f02776c1f4ea909f5371b7b65da2b
baselhegazi <baselmohammed226@gmail.com>
2025-03-12 10:46:10 +0200

    create it to match with figma design

---
+++ lib/features/_06_profile_view/presentation/view/widgets/profile_user_info.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/helper/const_variables.dart';
+import '../../../../../generated/assets.dart';
+
+class ProfileUserInfo extends StatelessWidget {
+  const ProfileUserInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 12,
+      children: [
+        Image.asset(
+          Assets.profileEllipse7938,
+          fit: BoxFit.cover,
+          height: 64,
+          width: 64,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Ahmed Ali',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: kTextPrimaryColor,
+                ),
+              ),
+              Text(
+                'ahmed123@gmail.com',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: kTextPrimaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
