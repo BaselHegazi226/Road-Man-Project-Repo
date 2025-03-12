a0ced2faa15b91437a92c1cb5a9a975ccd52c0c2
baselhegazi <baselmohammed226@gmail.com>
2025-03-12 10:45:28 +0200

    create it

---
+++ lib/features/_06_profile_view/presentation/view/profile_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/core/helper/class_const_functions.dart';
+import 'package:road_man_project/core/helper/const_variables.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/profile_view_body.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kAppPrimaryWhiteColor,
+      appBar: ConstFunctions.customAppBar(text: 'Profile', onPressed: () {}),
+      body: const ProfileViewBody(),
+    );
+  }
+}
