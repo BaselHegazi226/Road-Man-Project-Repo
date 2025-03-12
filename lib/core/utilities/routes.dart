3e2d7d21d0644885fce3ea4748e813a846579350
baselhegazi <baselmohammed226@gmail.com>
2025-03-12 15:19:11 +0200

    remove kProfileIconColor

--- lib/core/utilities/routes.dart
+++ lib/core/utilities/routes.dart
@@ -2,6 +2,5 @@ class Routes {
   static String forgetPasswordView = '/ForgetPasswordView';
   static String signUpView = '/SignUpView';
   static String signInView = '/SignInView';
-  static String questionnaireView = '/QuestionnaireView';
   static String homeView = '/homeView';
 }
