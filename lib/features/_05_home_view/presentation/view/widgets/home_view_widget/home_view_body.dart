b0d1a471b4045eb09fb1b94ac276eada854e1460
baselhegazi <baselmohammed226@gmail.com>
2025-03-12 10:45:03 +0200

    add ProfileView to tabBarView

--- lib/features/_05_home_view/presentation/view/widgets/home_view_widget/home_view_body.dart
+++ lib/features/_05_home_view/presentation/view/widgets/home_view_widget/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
 import 'package:road_man_project/features/_05_home_view/presentation/view/widgets/home_view_widget/custom_bottom_navigation_bar.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/profile_view.dart';
 
 class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
@@ -32,7 +33,7 @@ class _HomeViewBodyState extends State<HomeViewBody>
           Center(child: Text('Home', style: TextStyle(fontSize: 24))),
           Center(child: Text('Learn', style: TextStyle(fontSize: 24))),
           Center(child: Text('Job', style: TextStyle(fontSize: 24))),
-          Center(child: Text('Profile', style: TextStyle(fontSize: 24))),
+          ProfileView(),
         ],
       ),
       bottomNavigationBar: CustomBottomNavigationBar(
