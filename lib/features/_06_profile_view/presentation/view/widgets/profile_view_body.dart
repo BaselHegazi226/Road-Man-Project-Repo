ae2eb987fb729a81b15bd9627f6b54a8450fa2e7
baselhegazi <baselmohammed226@gmail.com>
2025-03-12 10:47:03 +0200

    call ProfileUserinfo

---
+++ lib/features/_06_profile_view/presentation/view/widgets/profile_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/profile_user_info.dart';
+
+class ProfileViewBody extends StatelessWidget {
+  const ProfileViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(children: [ProfileUserInfo()]),
+    );
+  }
+}
