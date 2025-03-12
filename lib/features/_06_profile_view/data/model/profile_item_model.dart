0177b7a36c31ef357f485499485fdb1ba31d179d
baselhegazi <baselmohammed226@gmail.com>
2025-03-12 17:22:04 +0200

    add onTap parameter for each model

--- lib/features/_06_profile_view/data/model/profile_item_model.dart
+++ lib/features/_06_profile_view/data/model/profile_item_model.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
 class ProfileItemModel {
-  final IconData suffixIcon, prefixIcon;
+  final IconData prefixIcon;
   final String title;
+  final Function()? onTap;
   ProfileItemModel({
+    required this.onTap,
     required this.prefixIcon,
-    required this.suffixIcon,
     required this.title,
   });
 }
