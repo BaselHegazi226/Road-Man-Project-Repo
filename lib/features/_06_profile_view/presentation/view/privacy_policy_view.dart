0e91dce2ea4db588f1269096f40eef9fb61de41d
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 07:27:55 +0200

    add body const PrivacyPolicyViewBody(),

--- lib/features/_06_profile_view/presentation/view/privacy_policy_view.dart
+++ lib/features/_06_profile_view/presentation/view/privacy_policy_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/04_privacy_policy_widgets/privacy_policy_view_body.dart';
 
 import '../../../../core/helper/class_const_functions.dart';
 import '../../../../core/helper/const_variables.dart';
@@ -16,6 +17,7 @@ class PrivacyPolicyView extends StatelessWidget {
         text: 'Privacy Policy',
         onPressed: () {},
       ),
+      body: const PrivacyPolicyViewBody(),
     );
   }
 }
