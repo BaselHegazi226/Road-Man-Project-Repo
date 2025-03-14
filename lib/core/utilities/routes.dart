b76ddc2da3c28a419eb7648fb074012f93b0bfd6
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 06:07:42 +0200

    add routes to 1- EditProfileView() 2- SettingsView() 3- AboutAppView() 4- PrivacyPolicyView() 5- FAQView()

--- lib/core/utilities/routes.dart
+++ lib/core/utilities/routes.dart
@@ -3,4 +3,9 @@ class Routes {
   static String signUpView = '/SignUpView';
   static String signInView = '/SignInView';
   static String homeView = '/homeView';
+  static String aboutAppView = '/aboutAppView';
+  static String editProfileView = '/editProfileView';
+  static String settingsView = '/settingsView';
+  static String privacyPolicyView = '/privacyPolicyView';
+  static String faqView = '/faqView';
 }
