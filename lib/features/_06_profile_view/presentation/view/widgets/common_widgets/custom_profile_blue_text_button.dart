e3124ac4d2e8ec60d9e381d74a59172e324c9304
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 07:56:13 +0200

    add text parameter , onPressed

--- lib/features/_06_profile_view/presentation/view/widgets/common_widgets/custom_profile_blue_text_button.dart
+++ lib/features/_06_profile_view/presentation/view/widgets/common_widgets/custom_profile_blue_text_button.dart
@@ -3,15 +3,20 @@ import 'package:flutter/material.dart';
 import '../../../../../../core/helper/const_variables.dart';
 
 class CustomProfileBlueTextButton extends StatelessWidget {
-  const CustomProfileBlueTextButton({super.key});
-
+  const CustomProfileBlueTextButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+  final String text;
+  final Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {},
-      child: const Text(
-        'I agree to the privacy policy',
-        style: TextStyle(
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: const TextStyle(
           color: kAppPrimaryBlueColor,
           fontSize: 16,
           fontWeight: FontWeight.w600,
