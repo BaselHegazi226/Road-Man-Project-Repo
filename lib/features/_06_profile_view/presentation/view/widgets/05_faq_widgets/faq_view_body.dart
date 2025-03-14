672ef4666499fe734427f5fcebc32f37fd8f563f
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 08:17:41 +0200

    create FaqViewBody.dart join : 1- FaqItemsSection 2- CustomProfileBlueTextButton

---
+++ lib/features/_06_profile_view/presentation/view/widgets/05_faq_widgets/faq_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/05_faq_widgets/faq_items_section.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/common_widgets/custom_profile_blue_text_button.dart';
+
+class FaqViewBody extends StatelessWidget {
+  const FaqViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+      child: Column(
+        spacing: 54,
+        children: [
+          const FaqItemsSection(),
+          CustomProfileBlueTextButton(text: 'Contact us', onPressed: () {}),
+        ],
+      ),
+    );
+  }
+}
