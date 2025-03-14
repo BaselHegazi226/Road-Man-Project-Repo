4d487b6b93600afa5a45956aed98da6fd8278f5b
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 06:07:16 +0200

    add go route to 1- EditProfileView() 2- SettingsView() 3- AboutAppView() 4- PrivacyPolicyView() 5- FAQView()

--- lib/core/utilities/app_router.dart
+++ lib/core/utilities/app_router.dart
@@ -1,6 +1,11 @@
 import 'package:go_router/go_router.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/forget_password_view.dart';
 import 'package:road_man_project/features/_05_home_view/presentation/view/home_view.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/about_app_view.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/edit_profile_view.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/faq_view.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/privacy_policy_view.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/settings_view.dart';
 
 import '../../features/_03_auth_view/presentation/views/sign_in_view.dart';
 import '../../features/_03_auth_view/presentation/views/sign_up_view.dart';
@@ -25,6 +30,26 @@ abstract class AppRouter {
       GoRoute(
         path: Routes.homeView,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: Routes.editProfileView,
+        builder: (context, state) => const EditProfileView(),
+      ),
+      GoRoute(
+        path: Routes.settingsView,
+        builder: (context, state) => const SettingsView(),
+      ),
+      GoRoute(
+        path: Routes.aboutAppView,
+        builder: (context, state) => const AboutAppView(),
+      ),
+      GoRoute(
+        path: Routes.privacyPolicyView,
+        builder: (context, state) => const PrivacyPolicyView(),
+      ),
+      GoRoute(
+        path: Routes.faqView,
+        builder: (context, state) => const FAQView(),
       ),
     ],
   );
