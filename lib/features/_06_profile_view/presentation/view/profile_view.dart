f06a6527147908281352fe3e0d23965345e4cbff
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 06:25:44 +0200

    put generalColor to custom app bar

--- lib/features/_06_profile_view/presentation/view/profile_view.dart
+++ lib/features/_06_profile_view/presentation/view/profile_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:road_man_project/core/helper/class_const_functions.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
-import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/profile_view_body.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/00_profile_widgets/profile_view_body.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -10,7 +10,11 @@ class ProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kAppPrimaryWhiteColor,
-      appBar: ConstFunctions.customAppBar(text: 'Profile', onPressed: () {}),
+      appBar: ConstFunctions.customAppBar(
+        generalColor: kTextPrimaryColor,
+        text: 'Profile',
+        onPressed: () {},
+      ),
       body: const ProfileViewBody(),
     );
   }
