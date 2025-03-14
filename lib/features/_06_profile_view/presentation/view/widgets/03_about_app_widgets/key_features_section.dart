977fb143fafec9a2378984e54807f3283778445d
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 06:21:17 +0200

    create key_features_section.dart joins: 1- text 2- KeyFeatureItemsSection()

---
+++ lib/features/_06_profile_view/presentation/view/widgets/03_about_app_widgets/key_features_section.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../core/helper/const_variables.dart';
+import 'key_feature_items_section.dart';
+
+class KeyFeaturesSection extends StatelessWidget {
+  const KeyFeaturesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 8,
+      children: [
+        Text(
+          'Key Features',
+          style: TextStyle(
+            color: kTextPrimaryColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        KeyFeatureItemsSection(),
+      ],
+    );
+  }
+}
