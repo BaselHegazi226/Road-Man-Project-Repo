ffde08f19d025f419197beb3ae7e198656c66180
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 08:16:46 +0200

    create privacy_policy_view_body.dart join : 1- CustomTitle 2- PrivacyPolicyItemsSection 3- CustomProfileBlueTextButton with Padding 48 from top

---
+++ lib/features/_06_profile_view/presentation/view/widgets/04_privacy_policy_widgets/privacy_policy_view_body.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/04_privacy_policy_widgets/privacy_policy_items_section.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/common_widgets/custom_profile_blue_text_button.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/common_widgets/custom_title.dart';
+
+class PrivacyPolicyViewBody extends StatelessWidget {
+  const PrivacyPolicyViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        spacing: 16,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const CustomTitle(title: 'Protecting your data is our priority'),
+          const PrivacyPolicyItemsSection(),
+          Padding(
+            padding: const EdgeInsets.only(top: 48),
+            child: CustomProfileBlueTextButton(
+              text: 'I agree to the privacy policy',
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
