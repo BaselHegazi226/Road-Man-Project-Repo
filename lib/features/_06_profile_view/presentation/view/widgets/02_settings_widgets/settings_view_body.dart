2b0b6cedbb8275a0b41a0e92eb30f8278d6b1a35
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 23:36:37 +0200

    create SettingsViewBody joins: 1- Column inside CustomFlexibleWidget inside padding: const EdgeInsets.only(left: 16, right: 16, top: 32), joins : 1- spacing 24 2- crossAxisAlignment: CrossAxisAlignment.start, 3- children : a- const LearningPreferencesSections(), b- const NotificationSection(), c- const CustomizeNotificationSection(), d- CustomTextButton inside   padding: const EdgeInsets.only(top: 48),

---
+++ lib/features/_06_profile_view/presentation/view/widgets/02_settings_widgets/settings_view_body.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/core/helper/const_variables.dart';
+import 'package:road_man_project/core/utilities/custom_flexible_widget.dart';
+import 'package:road_man_project/core/utilities/custom_text_button.dart';
+import 'package:road_man_project/core/utilities/custom_title.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/02_settings_widgets/learning_preferences_sections.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/02_settings_widgets/notification_section.dart';
+
+import 'customize_notification_section.dart';
+
+class SettingsViewBody extends StatelessWidget {
+  const SettingsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 32),
+      child: CustomFlexibleWidget(
+        child: Column(
+          spacing: 24,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const LearningPreferencesSections(),
+            const NotificationSection(),
+            const CustomizeNotificationSection(),
+            Padding(
+              padding: const EdgeInsets.only(top: 48),
+              child: CustomTextButton(
+                onPressed: () {},
+                backgroundColor: kAppPrimaryBlueColor,
+                child: CustomTitle(title: 'Update'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
