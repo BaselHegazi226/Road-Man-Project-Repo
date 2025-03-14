85948f26735fb210d50a1a4964b4eb4a48965780
baselhegazi <baselmohammed226@gmail.com>
2025-03-14 17:11:01 +0200

    create custom_icon_title.dart to create component of settings_view_body.dart joins: 1- row with spacing 8 joins: a- icon b- title

---
+++ lib/features/_06_profile_view/presentation/view/widgets/02_settings_widgets/custom_icon_title.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/core/helper/const_variables.dart';
+
+class CustomIconTitle extends StatelessWidget {
+  const CustomIconTitle({
+    super.key,
+    required this.iconData,
+    required this.title,
+  });
+  final IconData iconData;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      spacing: 8,
+      children: [
+        Icon(iconData, size: 24, color: kIconSettingsColor),
+        Text(
+          title,
+          style: TextStyle(
+            color: kTextPrimaryColor,
+            fontWeight: FontWeight.w600,
+            fontSize: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
