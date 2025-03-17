7aabd3d74ee91fb5ff87f7031afc8548de089267
baselhegazi <baselmohammed226@gmail.com>
2025-03-17 17:26:03 +0200

    instead EditProfileContainerSection with EditProfileNamesFieldsSection

--- lib/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_view_body.dart
+++ lib/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_container_section.dart';
 import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_image.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_names_fields_section.dart';
 import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_password_fields_section.dart';
 
 class EditProfileViewBody extends StatelessWidget {
@@ -14,7 +14,7 @@ class EditProfileViewBody extends StatelessWidget {
         spacing: 16,
         children: [
           EditProfileImage(),
-          EditProfileContainerSection(),
+          EditProfileNamesFieldsSection(),
           EditProfilePasswordFieldsSection(),
         ],
       ),
