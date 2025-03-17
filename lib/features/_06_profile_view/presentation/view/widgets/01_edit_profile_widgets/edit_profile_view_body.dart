4333133abbc705779f3aa1b8e312a8e9f7048c12
baselhegazi <baselmohammed226@gmail.com>
2025-03-17 08:40:40 +0200

    create edit_profile_view_body. with spacing 16 , joins: 1- EditProfileImage 2- EditProfileContainerSection 3- EditProfilePasswordFieldsSection

---
+++ lib/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_container_section.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_image.dart';
+import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_password_fields_section.dart';
+
+class EditProfileViewBody extends StatelessWidget {
+  const EditProfileViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+      child: Column(
+        spacing: 16,
+        children: [
+          EditProfileImage(),
+          EditProfileContainerSection(),
+          EditProfilePasswordFieldsSection(),
+        ],
+      ),
+    );
+  }
+}
