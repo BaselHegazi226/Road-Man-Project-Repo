8921ff5d438118007bf7f6a61fe105b900e3d0c0
baselhegazi <baselmohammed226@gmail.com>
2025-03-17 22:09:42 +0200

    instead the column with listview, transfer the logOutItemsSectionList to home_list_class

--- lib/features/_05_home_view/presentation/view/widgets/06_logout_widgets/log_out_items_section.dart
+++ lib/features/_05_home_view/presentation/view/widgets/06_logout_widgets/log_out_items_section.dart
@@ -1,50 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:road_man_project/features/_05_home_view/data/model/logout_item_model.dart';
+import 'package:go_router/go_router.dart';
 
+import '../home_common_widgets/home_const_list.dart';
 import 'logout_item.dart';
 
 class LogOutItemsSection extends StatelessWidget {
   const LogOutItemsSection({super.key});
   @override
   Widget build(BuildContext context) {
-    final List<LogoutItemModel> logOutItemsSectionList = [
-      LogoutItemModel(
-        onTap: () {},
-        prefixIcon: Icons.person_outline_outlined,
-        title: 'Profile',
-      ),
-      LogoutItemModel(
-        onTap: () {},
-        prefixIcon: Icons.local_library_outlined,
-        title: 'Learning preferences',
-      ),
-      LogoutItemModel(onTap: () {}, prefixIcon: Icons.bookmark, title: 'Saved'),
-      LogoutItemModel(
-        onTap: () {},
-        prefixIcon: Icons.settings,
-        title: 'Settings',
-      ),
-      LogoutItemModel(
-        onTap: () {},
-        prefixIcon: Icons.info_outlined,
-        title: 'About app',
-      ),
-      LogoutItemModel(
-        onTap: () {},
-        prefixIcon: Icons.privacy_tip_outlined,
-        title: 'Privacy policy',
-      ),
-      LogoutItemModel(
-        onTap: () {},
-        prefixIcon: Icons.help_outline_outlined,
-        title: 'FAQ',
-      ),
-    ];
-    return Column(
-      children:
-          logOutItemsSectionList
-              .map((item) => LogoutItem(logoutItemModel: item))
-              .toList(),
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: HomeConstList.logOutItemsSectionList.length,
+      itemBuilder: (context, index) {
+        final item = HomeConstList.logOutItemsSectionList[index];
+        return LogoutItem(
+          logoutItemModel: item,
+          onTap:
+              item.routeName.isNotEmpty
+                  ? () => GoRouter.of(context).push(item.routeName)
+                  : null,
+        );
+      },
     );
   }
 }
