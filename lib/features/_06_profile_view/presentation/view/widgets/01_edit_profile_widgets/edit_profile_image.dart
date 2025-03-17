54401747d69baabead4c08c47234cc46b7f9b3f2
baselhegazi <baselmohammed226@gmail.com>
2025-03-17 08:33:55 +0200

    create edit_profile_image.dart to put the profile image in stack and put also Icons.camera_alt_outlined, above it

---
+++ lib/features/_06_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_image.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../generated/assets.dart';
+
+class EditProfileImage extends StatelessWidget {
+  const EditProfileImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomRight,
+      children: [
+        Image.asset(
+          Assets.profileUserImage,
+          fit: BoxFit.cover,
+          height: 88,
+          width: 88,
+        ),
+        ClipRect(
+          clipBehavior: Clip.none,
+          child: Positioned(
+            bottom: 8,
+            right: 4,
+            child: Container(
+              padding: EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                color: Color(0xffE6E8EE),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.camera_alt_outlined,
+                size: 20,
+                color: Color(0xff131314),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
