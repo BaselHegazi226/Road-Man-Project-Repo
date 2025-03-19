e673ea15fc647674a0288db1853fb2662c6c22a4
baselhegazi <baselmohammed226@gmail.com>
2025-03-19 07:22:53 +0200

    add jobView()

--- lib/features/_05_home_view/presentation/view/widgets/home_view_widget/home_view_body.dart
+++ lib/features/_05_home_view/presentation/view/widgets/home_view_widget/home_view_body.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
 import 'package:road_man_project/features/_05_home_view/presentation/view/widgets/home_view_widget/custom_bottom_navigation_bar.dart';
 import 'package:road_man_project/features/_06_profile_view/presentation/view/profile_view.dart';
-import 'package:road_man_project/features/_08_job_view/presentation/view/job_view.dart';
+
+import '../../../../../_08_job_view/presentation/view/job_view.dart';
 
 class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
