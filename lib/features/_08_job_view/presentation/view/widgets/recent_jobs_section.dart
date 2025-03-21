613cbdbcb759feaec56167cd3826c44725b72011
baselhegazi <baselmohammed226@gmail.com>
2025-03-21 14:52:41 +0200

    feat: Add RecentJobsSection with job categories and listings
    - Created `RecentJobsSection` to organize job-related components.
    - Used `Expanded` to make it adapt within available space.
    - Included `TwoTitle` for section heading with an action button.
    - Added `RecentJobsItemListView` for job categories.
    - Integrated `RecentJobsCardListView` for displaying job cards.
    - Used `Column` for structured vertical layout.

---
+++ lib/features/_08_job_view/presentation/view/widgets/recent_jobs_section.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/recent_jobs_card_list_view.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/recent_jobs_item_list_view.dart';
+
+import 'two_title.dart';
+
+class RecentJobsSection extends StatelessWidget {
+  const RecentJobsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        spacing: 16,
+        children: [
+          TwoTitle(
+            title1: 'Recent Jobs',
+            title2: 'View all',
+            title2onPressed: () {},
+          ),
+          RecentJobsItemListView(),
+          RecentJobsCardListView(),
+        ],
+      ),
+    );
+  }
+}
