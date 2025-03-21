ee7f8bc757ff4adbb2ae67cb6230f1e0caa7791f
baselhegazi <baselmohammed226@gmail.com>
2025-03-21 14:48:47 +0200

    feat: Add RecentJobsCardPriceSection with salary and job type
    - Created `RecentJobsCardPriceSection` for salary and job type display.
    - Used `Row` for horizontal alignment of salary and job type.
    - Styled salary text with custom color and font.
    - Added a bordered `Container` for job type label.

---
+++ lib/features/_08_job_view/presentation/view/widgets/recent_jobs_card_price_section.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/helper/const_variables.dart';
+
+class RecentJobsCardPriceSection extends StatelessWidget {
+  const RecentJobsCardPriceSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 8,
+      children: [
+        Text(
+          '\$42 - \$48k /month',
+          style: TextStyle(
+            color: Color(0xff8A8C90),
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(color: Color(0xff8A8C90), width: 1.5),
+          ),
+          child: Text(
+            'Full-Time',
+            style: TextStyle(
+              color: kTextPrimaryColor,
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
