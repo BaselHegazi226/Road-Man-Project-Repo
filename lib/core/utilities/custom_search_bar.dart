b7429a6120b7af2444e840ded9dcbd42859c776d
baselhegazi <baselmohammed226@gmail.com>
2025-03-21 14:09:32 +0200

    create custom_search_bar to reuse it after i more views

---
+++ lib/core/utilities/custom_search_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../helper/const_variables.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({
+    super.key,
+    required this.searchBarEditingController,
+    required this.searchFocusNode,
+    required this.hintText,
+  });
+  final TextEditingController searchBarEditingController;
+  final FocusNode searchFocusNode;
+  final String hintText;
+  @override
+  Widget build(BuildContext context) {
+    return SearchBar(
+      padding: WidgetStatePropertyAll(EdgeInsets.symmetric(horizontal: 8)),
+      backgroundColor: WidgetStateProperty.all(Color(0xffE6E8EE)),
+      hintText: hintText,
+      hintStyle: WidgetStatePropertyAll(
+        TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 15,
+          letterSpacing: -.285,
+          height: 1.5,
+          color: Color.fromRGBO(19, 19, 20, 0.50),
+        ),
+      ),
+      leading: Icon(
+        Icons.search_outlined,
+        color: kTextPrimaryColor.withAlpha(126),
+        size: 24,
+      ),
+      elevation: WidgetStatePropertyAll(0),
+      controller: searchBarEditingController,
+      focusNode: searchFocusNode,
+    );
+  }
+}
