c398bbc690861c9c526545fcce620b6d6fdecfaa
baselhegazi <baselmohammed226@gmail.com>
2025-03-21 14:10:28 +0200

    create job_view_app_bar to put it in job_view appbar

---
+++ lib/features/_08_job_view/presentation/view/widgets/job_view_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/helper/const_variables.dart';
+
+AppBar jobViewAppbar() {
+  return AppBar(
+    backgroundColor: Color(0xffF4F5F8),
+    leading: IconButton(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      icon: const Icon(Icons.menu_outlined, color: kTextPrimaryColor, size: 24),
+      onPressed: () {},
+    ),
+    actions: [
+      IconButton(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        icon: const Icon(
+          Icons.notifications_outlined,
+          color: kTextPrimaryColor,
+          size: 24,
+        ),
+        onPressed: () {},
+      ),
+    ],
+  );
+}
