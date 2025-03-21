31176354ce95cdd9bf221419cefd9f4f2bd0e75b
baselhegazi <baselmohammed226@gmail.com>
2025-03-21 14:11:46 +0200

    make the heights, widths more responsive and use custom_search_bar

--- lib/features/_08_job_view/presentation/view/widgets/job_view_search_bar_section.dart
+++ lib/features/_08_job_view/presentation/view/widgets/job_view_search_bar_section.dart
@@ -1,41 +1,30 @@
 import 'package:flutter/material.dart';
-
-import '../../../../../core/helper/const_variables.dart';
+import 'package:road_man_project/core/utilities/custom_search_bar.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/search_bar_section_icon.dart';
 
 class JobViewSearchBarSection extends StatelessWidget {
   const JobViewSearchBarSection({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
-      height: 40,
-      margin: EdgeInsets.only(top: 16),
+      height: screenHeight * 0.045,
+      margin: EdgeInsets.only(top: screenHeight * 0.02),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        spacing: 8,
+        spacing: screenWidth * 0.02,
         children: [
           Expanded(
-            child: SearchBar(
-              backgroundColor: WidgetStateProperty.all(Color(0xffE6E8EE)),
+            child: CustomSearchBar(
+              searchBarEditingController: TextEditingController(),
+              searchFocusNode: FocusNode(),
               hintText: 'Job title',
-              leading: Icon(Icons.search_outlined),
-              hintStyle: WidgetStatePropertyAll(TextStyle()),
-              elevation: WidgetStatePropertyAll(0),
             ),
           ),
-          Container(
-            width: 40,
-            height: 40,
-            decoration: BoxDecoration(
-              color: kAppPrimaryBlueColor,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Icon(
-              Icons.tune_outlined,
-              color: kAppPrimaryWhiteColor,
-              size: 24,
-            ),
-          ),
+          SearchBarSectionIcon(iconData: Icons.tune_outlined),
         ],
       ),
     );
