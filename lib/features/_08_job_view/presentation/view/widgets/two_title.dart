95f30800e136ad1a34cc09d8298af77519d60b38
baselhegazi <baselmohammed226@gmail.com>
2025-03-21 14:14:27 +0200

    create twoTitleWidget joins: 1- title in the start of screen take black grade color 2- title in the end of screen take blue grade color

---
+++ lib/features/_08_job_view/presentation/view/widgets/two_title.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/helper/const_variables.dart';
+
+class TwoTitle extends StatelessWidget {
+  const TwoTitle({
+    super.key,
+    required this.title1,
+    required this.title2,
+    required this.title2onPressed,
+  });
+  final String title1, title2;
+  final VoidCallback? title2onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title1,
+          style: TextStyle(
+            color: kTextPrimaryColor,
+            fontWeight: FontWeight.w500,
+            fontSize: 15,
+          ),
+        ),
+        TextButton(
+          onPressed: title2onPressed,
+          child: Text(
+            title2,
+            style: TextStyle(
+              color: kAppPrimaryBlueColor,
+              fontWeight: FontWeight.w500,
+              fontSize: 15,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
