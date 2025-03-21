6e996a40d1500ab61fda0a0ec89718359de73c82
baselhegazi <baselmohammed226@gmail.com>
2025-03-21 14:26:04 +0200

    create suggested_jobs_item joins: 1- container with decoration joins: colum with spacing joins: 1- track_location_section 2- PriceMonthlySubscriptionSection 3- WorkingTimeSection with top padding 16

---
+++ lib/features/_08_job_view/presentation/view/widgets/suggested_jobs_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/price_monthly_subscription_section.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/track_location_section.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/working_time_item_section.dart';
+
+class SuggestedJobsItem extends StatelessWidget {
+  const SuggestedJobsItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 182,
+      width: MediaQuery.sizeOf(context).width * .85,
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      decoration: BoxDecoration(
+        color: Color(0xffE6E8EE),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        spacing: 16,
+        children: [
+          TrackLocationSection(),
+          PriceMonthlySubscriptionSection(),
+          Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: WorkingTimeSection(),
+          ),
+        ],
+      ),
+    );
+  }
+}
