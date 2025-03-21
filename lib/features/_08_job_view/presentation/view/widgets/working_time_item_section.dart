c7efeb1f3f3a33218d5bb709d7273e58f66960f1
baselhegazi <baselmohammed226@gmail.com>
2025-03-21 14:24:30 +0200

    create working_time_item_section joins: 1- row with spacing 16 joins: a- row with spacing 16 joins: i- tow working_time_time ii- working_time_item_arrow

---
+++ lib/features/_08_job_view/presentation/view/widgets/working_time_item_section.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/working_time_item.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/working_time_item_arrow.dart';
+
+class WorkingTimeSection extends StatelessWidget {
+  const WorkingTimeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 16,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          spacing: 16,
+          children: [
+            WorkingTimeItem(title: 'Part_Time'),
+            WorkingTimeItem(title: 'Advanced Level'),
+          ],
+        ),
+        WorkingTimeItemArrow(),
+      ],
+    );
+  }
+}
