2b0f02d061cce98b896626f9da53ab83a6cec7f0
baselhegazi <baselmohammed226@gmail.com>
2025-03-22 17:11:52 +0200

    rename it to DashboardItemModel

--- lib/features/_05_home_view/data/model/dashboard_item_model.dart
+++ lib/features/_05_home_view/data/model/dashboard_item_model.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
 
-class LogoutItemModel {
+class DashboardItemModel {
   final IconData prefixIcon;
   final String title;
   final Color iconColor, textColor;
   final String routeName;
-  const LogoutItemModel({
+  const DashboardItemModel({
     required this.routeName,
     required this.prefixIcon,
     required this.title,
