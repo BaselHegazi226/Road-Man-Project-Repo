6eb02a09ea4b3f058d95622ef0a55ef944a39bbb
baselhegazi <baselmohammed226@gmail.com>
2025-03-22 03:45:44 +0200

    feat: Implement JobDetailsView screen
    - Created `JobDetailsView` as a `StatelessWidget`.
    - Used `Scaffold` with `kAppPrimaryBlueColor` background.
    - Integrated `jobDetailsAppbar(context)` for a custom app bar.
    - Added `JobDetailsViewBody` wrapped in `Padding` with a top margin of `64`.

---
+++ lib/features/_08_job_view/presentation/view/job_details_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/core/helper/const_variables.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_app_bar.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_view_body.dart';
+
+class JobDetailsView extends StatelessWidget {
+  const JobDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kAppPrimaryBlueColor,
+      appBar: jobDetailsAppbar(context),
+      body: Padding(
+        padding: EdgeInsets.only(top: 64),
+        child: JobDetailsViewBody(),
+      ),
+    );
+  }
+}
