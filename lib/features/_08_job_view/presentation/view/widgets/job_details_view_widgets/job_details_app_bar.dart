567353ffe93f739c1921ff53bce93bd173d5b4bc
baselhegazi <baselmohammed226@gmail.com>
2025-03-22 17:33:55 +0200

    add openDashboard(context, const JobViewDashboardViewBody()); to menu_outlined icon button

--- lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_app_bar.dart
+++ lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_app_bar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:road_man_project/features/_05_home_view/presentation/view/widgets/custom_dashboard_widgets/open_dashboard.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/job_view_widgets/job_view_dashboard_view_body.dart';
 
 import '../../../../../../core/helper/const_variables.dart';
 import '../../../../../../core/utilities/base_text_styles.dart';
@@ -18,7 +20,9 @@ AppBar jobDetailsAppbar(BuildContext context) {
     actions: [
       IconButton(
         padding: EdgeInsets.symmetric(horizontal: 16),
-        onPressed: () {},
+        onPressed: () {
+          openDashboard(context, const JobViewDashboardViewBody());
+        },
         icon: Icon(Icons.menu_outlined, color: kSecondlyLightWhiteColor),
       ),
     ],
