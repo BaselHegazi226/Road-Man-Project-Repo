cf43927192c4540e463baebdd2eec895e6cb4454
baselhegazi <baselmohammed226@gmail.com>
2025-03-22 03:56:18 +0200

    feat: Add JobDetailsViewBody with structured layout and Google icon positioning
    - Introduced `JobDetailsViewBody` as the main container for job details.
    - Applied `Expanded` to ensure proper layout expansion within the parent widget.
    - Utilized `Stack` for precise positioning of `JobDetailsGoogleIcon` and `JobDetailsComponentSection`.
    - Integrated `CustomFlexibleWidget` to enhance UI flexibility and maintain responsiveness.
    - Applied `BoxDecoration` with rounded corners for a polished UI appearance.
    
    This commit improves UI consistency, optimizes layout structure, and enhances visual presentation.

---
+++ lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_view_body.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/core/helper/const_variables.dart';
+import 'package:road_man_project/core/utilities/custom_flexible_widget.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_component_section.dart';
+
+import 'job_details_goole_icon.dart';
+
+class JobDetailsViewBody extends StatelessWidget {
+  const JobDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: kAppPrimaryWhiteColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(48),
+            topRight: Radius.circular(48),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.topCenter,
+            children: [
+              Positioned(top: -32, child: JobDetailsGoogleIcon()),
+              Positioned(
+                top: 46,
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: CustomFlexibleWidget(
+                  child: JobDetailsComponentSection(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
