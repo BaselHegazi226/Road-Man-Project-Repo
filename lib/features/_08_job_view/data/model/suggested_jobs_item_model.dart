80f90f6ca5eb428bf225d8e5470002837c45ad48
baselhegazi <baselmohammed226@gmail.com>
2025-03-23 15:11:57 +0200

    create SuggestedJobsItemModel to use it in jobView in section SuggestedJobs

---
+++ lib/features/_08_job_view/data/model/suggested_jobs_item_model.dart
@@ -0,0 +1,15 @@
+class SuggestedJobsItemModel {
+  final String trackLocationImage;
+  final String trackLocationTitle, trackLocationSubtitle;
+  final String price, subscriptionType;
+  final String workingTimeSectionTitle1, workingTimeSectionTitle2;
+  const SuggestedJobsItemModel({
+    required this.trackLocationImage,
+    required this.trackLocationTitle,
+    required this.trackLocationSubtitle,
+    required this.price,
+    required this.subscriptionType,
+    required this.workingTimeSectionTitle1,
+    required this.workingTimeSectionTitle2,
+  });
+}
