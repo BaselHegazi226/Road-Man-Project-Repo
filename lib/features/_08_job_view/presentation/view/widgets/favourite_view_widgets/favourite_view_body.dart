3425e1908d130231dd5614e35153432ce744a469
baselhegazi <baselmohammed226@gmail.com>
2025-03-23 18:05:14 +0200

    feat: Add FavouriteViewBody widget
    - Created `FavouriteViewBody` as a stateless widget.
    - Added horizontal padding for consistent spacing.
    - Set up a `Column` for future content expansion.

---
+++ lib/features/_08_job_view/presentation/view/widgets/favourite_view_widgets/favourite_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FavouriteViewBody extends StatelessWidget {
+  const FavouriteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(),
+    );
+  }
+}
