dcc6880e431451d8cce298b983589ed1775075d9
baselhegazi <baselmohammed226@gmail.com>
2025-03-23 18:04:50 +0200

    feat: Add custom AppBar for FavouriteView
    - Implemented `favouriteViewAppBar` with a back button using `GoRouter.pop()`.
    - Centered the title with consistent text styling.
    - Added a profile image as an action button for better UI/UX.
    - Ensured theme consistency with `kAppPrimaryWhiteColor`.

---
+++ lib/features/_08_job_view/presentation/view/widgets/favourite_view_widgets/favourite_view_app_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../../../core/helper/const_variables.dart';
+import '../../../../../../core/utilities/base_text_styles.dart';
+import '../../../../../../generated/assets.dart';
+
+AppBar favouriteViewAppBar(BuildContext context) {
+  return AppBar(
+    backgroundColor: kAppPrimaryWhiteColor,
+    title: Text(
+      'Favourite items',
+      style: AfacadTextStyles.textStyle20W500Black,
+    ),
+    centerTitle: true,
+    leading: IconButton(
+      onPressed: () {
+        GoRouter.of(context).pop();
+      },
+      icon: Icon(CupertinoIcons.back, color: kEditProfileIconColor),
+    ),
+    actions: [
+      Padding(
+        padding: EdgeInsets.only(right: 16),
+        child: Image.asset(
+          Assets.profileProfileUserImage,
+          width: 32,
+          height: 32,
+        ),
+      ),
+    ],
+  );
+}
