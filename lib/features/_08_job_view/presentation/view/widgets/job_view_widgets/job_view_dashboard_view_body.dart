e38bd1a66a62bd65916c7dab10101cab87aabbc4
baselhegazi <baselmohammed226@gmail.com>
2025-03-23 18:11:44 +0200

    change padding to  padding: const EdgeInsets.only(left: 16, right: 16, top: 64, bottom: 32),

--- lib/features/_08_job_view/presentation/view/widgets/job_view_widgets/job_view_dashboard_view_body.dart
+++ lib/features/_08_job_view/presentation/view/widgets/job_view_widgets/job_view_dashboard_view_body.dart
@@ -3,7 +3,7 @@ import 'package:road_man_project/core/helper/const_variables.dart';
 import 'package:road_man_project/features/_05_home_view/presentation/view/widgets/custom_dashboard_widgets/custom_dashboard_icon_title.dart';
 
 import '../../../../../_06_profile_view/presentation/view/widgets/profile_common_widgets/profile_user_info.dart';
-import 'job_view_drawer_items_section.dart';
+import 'job_view_dashboard_items_section.dart';
 
 class JobViewDashboardViewBody extends StatelessWidget {
   const JobViewDashboardViewBody({super.key});
@@ -11,13 +11,13 @@ class JobViewDashboardViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 64, bottom: 32),
       child: Column(
         spacing: 8,
         children: [
           const ProfileUserInfo(),
           Divider(thickness: .5, color: kLogoutLineColor),
-          const JobViewDrawerItemsSection(),
+          const JobViewDashboardItemsSection(),
           const Spacer(),
           Divider(thickness: .5, color: kLogoutLineColor),
           CustomDrawerIconTitle(
