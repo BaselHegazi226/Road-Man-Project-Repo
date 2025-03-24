3493f3bf0c9991d490bfe5c97835e8ac3824f7a4
baselhegazi <baselmohammed226@gmail.com>
2025-03-24 17:47:53 +0200

    Updated FavouriteViewBody: Added FavouriteCardListView inside Expanded

--- lib/features/_08_job_view/presentation/view/widgets/favourite_view_widgets/favourite_view_body.dart
+++ lib/features/_08_job_view/presentation/view/widgets/favourite_view_widgets/favourite_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/favourite_view_widgets/favourite_card_list_view.dart';
 
 class FavouriteViewBody extends StatelessWidget {
   const FavouriteViewBody({super.key});
@@ -7,7 +8,7 @@ class FavouriteViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(),
+      child: Expanded(child: FavouriteCardListView()),
     );
   }
 }
