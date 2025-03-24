8121351c51b773849a4afd4a07d031ed7ddee243
baselhegazi <baselmohammed226@gmail.com>
2025-03-24 17:55:59 +0200

    - Changed `SuggestedJobsCardModel` to `JobViewCardModel` for consistency. - Updated the `suggestedJobsItemModelList` type to `List<JobViewCardModel>`. - No functional changes, but note that `Expanded` inside `SizedBox` is unnecessary and should be removed.

--- lib/features/_08_job_view/presentation/view/widgets/suggested_jobs_widgets/suggested_jobs_card_list_view.dart
+++ lib/features/_08_job_view/presentation/view/widgets/suggested_jobs_widgets/suggested_jobs_card_list_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:road_man_project/features/_08_job_view/data/model/suggested_jobs_card_model.dart';
+import 'package:road_man_project/features/_08_job_view/data/model/job_view_card_model.dart';
 import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/suggested_jobs_widgets/suggested_jobs_card.dart';
 
 class SuggestedJobsCardListView extends StatelessWidget {
@@ -7,7 +7,7 @@ class SuggestedJobsCardListView extends StatelessWidget {
     super.key,
     required this.suggestedJobsItemModelList,
   });
-  final List<SuggestedJobsCardModel> suggestedJobsItemModelList;
+  final List<JobViewCardModel> suggestedJobsItemModelList;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
