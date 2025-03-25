af34884418081cc0441624cd91370080e525a1c5
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 14:20:45 +0200

    feat: Make SuggestedJobsCard responsive
    - Adjusted height and width dynamically using screen dimensions.
    - Made padding and border radius responsive for better UI scaling.
    - Used dynamic spacing to improve layout consistency.
    - Enhanced

--- lib/features/_08_job_view/presentation/view/widgets/suggested_jobs_widgets/suggested_jobs_card.dart
+++ lib/features/_08_job_view/presentation/view/widgets/suggested_jobs_widgets/suggested_jobs_card.dart
@@ -9,8 +9,12 @@ import 'package:road_man_project/features/_08_job_view/presentation/view/widgets
 class SuggestedJobsCard extends StatelessWidget {
   const SuggestedJobsCard({super.key, required this.suggestedJobsItemModel});
   final JobViewCardModel suggestedJobsItemModel;
+
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return InkWell(
       onTap: () {
         GoRouter.of(
@@ -18,15 +22,20 @@ class SuggestedJobsCard extends StatelessWidget {
         ).push(Routes.jobDetailsViewId, extra: suggestedJobsItemModel);
       },
       child: Container(
-        height: 182,
-        width: MediaQuery.sizeOf(context).width * .85,
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        height: screenHeight * 0.22, // جعل الارتفاع ديناميكيًا
+        width: screenWidth * 0.85, // جعل العرض ديناميكيًا
+        padding: EdgeInsets.symmetric(
+          horizontal: screenWidth * 0.02,
+          vertical: screenHeight * 0.017,
+        ),
         decoration: BoxDecoration(
-          color: Color(0xffE6E8EE),
-          borderRadius: BorderRadius.circular(16),
+          color: const Color(0xffE6E8EE),
+          borderRadius: BorderRadius.circular(
+            screenWidth * 0.04,
+          ), // جعل الحواف ديناميكية
         ),
         child: Column(
-          spacing: 16,
+          spacing: screenHeight * 0.015, // تباعد ديناميكي بين العناصر
           children: [
             SuggestedJobsTrackLocationSection(
               title: suggestedJobsItemModel.title,
@@ -40,7 +49,9 @@ class SuggestedJobsCard extends StatelessWidget {
               subscriptionType: suggestedJobsItemModel.salaryType,
             ),
             Padding(
-              padding: EdgeInsets.only(top: 16),
+              padding: EdgeInsets.only(
+                top: screenHeight * 0.015,
+              ), // تباعد ديناميكي
               child: WorkingTimeSection(
                 jobType: suggestedJobsItemModel.jobType,
                 jobLevel: suggestedJobsItemModel.jobLevel!,
