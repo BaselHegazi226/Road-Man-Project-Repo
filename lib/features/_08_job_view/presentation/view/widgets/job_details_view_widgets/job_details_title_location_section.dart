c40d2cfdd9e815974db567811d3d5b014aa4d623
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 13:23:02 +0200

    refactor: make JobDetailsTitleLocationSection dynamic
    - Added parameters for title, company, and location.
    - Removed hardcoded text to allow dynamic content.
    - Ensured text styling remains consistent.

--- lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_title_location_section.dart
+++ lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_title_location_section.dart
@@ -2,24 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:road_man_project/core/utilities/base_text_styles.dart';
 
 class JobDetailsTitleLocationSection extends StatelessWidget {
-  const JobDetailsTitleLocationSection({super.key});
-
+  const JobDetailsTitleLocationSection({
+    super.key,
+    required this.title,
+    required this.company,
+    required this.location,
+  });
+  final String title, company, location;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       spacing: 4,
       children: [
-        Text(
-          'Front End Developer',
-          style: AfacadTextStyles.textStyle20W600Black,
-        ),
+        Text(title, style: AfacadTextStyles.textStyle20W600Black),
         Row(
           spacing: 8,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Google, Inc', style: AfacadTextStyles.textStyle16W400Grey),
-            Text('Cairo, Egypt', style: AfacadTextStyles.textStyle16W400Grey),
+            Text(company, style: AfacadTextStyles.textStyle16W400Grey),
+            Text(location, style: AfacadTextStyles.textStyle16W400Grey),
           ],
         ),
       ],
