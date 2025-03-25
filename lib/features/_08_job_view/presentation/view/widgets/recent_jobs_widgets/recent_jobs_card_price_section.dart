26c3d9f9f0febfa5e2048f0594b5346e4737adaf
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 17:17:19 +0200

    - Replaced fixed spacing (`8`) with `screenWidth * 0.02` for responsiveness. - Made padding inside the container responsive using `screenWidth * 0.02` and `screenHeight * 0.005`. - Adjusted border radius dynamically with `screenWidth * 0.02`.

--- lib/features/_08_job_view/presentation/view/widgets/recent_jobs_widgets/recent_jobs_card_price_section.dart
+++ lib/features/_08_job_view/presentation/view/widgets/recent_jobs_widgets/recent_jobs_card_price_section.dart
@@ -4,6 +4,7 @@ import 'package:road_man_project/core/utilities/base_text_styles.dart';
 class RecentJobsCardPriceSection extends StatelessWidget {
   final String salary;
   final String jobType;
+
   const RecentJobsCardPriceSection({
     super.key,
     required this.salary,
@@ -12,14 +13,20 @@ class RecentJobsCardPriceSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Row(
-      spacing: 8,
+      spacing: screenWidth * 0.02,
       children: [
         Text(salary, style: AfacadTextStyles.textStyle14W400Grey),
         Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          padding: EdgeInsets.symmetric(
+            horizontal: screenWidth * 0.02,
+            vertical: screenHeight * 0.005,
+          ),
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
+            borderRadius: BorderRadius.circular(screenWidth * 0.02),
             border: Border.all(color: const Color(0xff8A8C90), width: 1.5),
           ),
           child: Text(jobType, style: AfacadTextStyles.textStyle14W400Black),
