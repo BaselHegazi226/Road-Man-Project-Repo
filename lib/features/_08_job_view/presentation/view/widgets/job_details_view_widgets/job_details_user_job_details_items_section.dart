c793629cfc4fed64fec1962d63f69a7424655e08
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 13:21:53 +0200

    refactor: update JobDetailsUserJobDetailsItemsSection to use JobViewCardModel
    - Removed static job details list and replaced it with dynamic data from JobViewCardModel.
    - Modified the layout from GridView to Column with Rows for better UI structure.
    - Added icons and improved readability of job details.

--- lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_user_job_details_items_section.dart
+++ lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_user_job_details_items_section.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:road_man_project/features/_08_job_view/data/model/job_view_card_model.dart';
 
-import '../job_view_widgets/const_job_details_list_class.dart';
 import 'job_details_user_job_details_item.dart';
 
 class JobDetailsUserJobDetailsItemsSection extends StatelessWidget {
-  const JobDetailsUserJobDetailsItemsSection({super.key});
-
+  const JobDetailsUserJobDetailsItemsSection({
+    super.key,
+    required this.jobViewCardModel,
+  });
+  final JobViewCardModel jobViewCardModel;
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 4,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 16,
-        mainAxisSpacing: 12,
-        childAspectRatio: 1.5,
-      ),
-      physics: const NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemBuilder: (context, index) {
-        return JobDetailsUserJobDetailsItem(
-          jobDetailsUserJobDetailsItemModel:
-              ConstJobDetailsListClass
-                  .jobDetailsUserJobDetailsItemModelList[index],
-        );
-      },
+    return Column(
+      spacing: 16,
+      children: [
+        Row(
+          spacing: 13,
+          children: [
+            JobDetailsUserJobDetailsItem(
+              iconData: Icons.attach_money_outlined,
+              title: jobViewCardModel.salaryType,
+              subTitle: jobViewCardModel.salary,
+            ),
+            JobDetailsUserJobDetailsItem(
+              iconData: Icons.work_history_outlined,
+              title: 'Job Type',
+              subTitle: jobViewCardModel.jobType,
+            ),
+          ],
+        ),
+        Row(
+          spacing: 13,
+          children: [
+            JobDetailsUserJobDetailsItem(
+              iconData: Icons.add_location_alt_outlined,
+              title: 'Work Model',
+              subTitle: jobViewCardModel.location,
+            ),
+            JobDetailsUserJobDetailsItem(
+              iconData: FontAwesomeIcons.stairs,
+              title: 'Level',
+              subTitle: jobViewCardModel.jobLevel ?? 'suggested',
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
