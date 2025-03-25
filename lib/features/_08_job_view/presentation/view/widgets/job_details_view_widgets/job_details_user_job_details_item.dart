d5f6c84a31a8d42b910311743ff1b250dde88004
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 13:22:24 +0200

    refactor: update JobDetailsUserJobDetailsItem to use direct properties
    - Removed dependency on JobDetailsUserJobDetailsItemModel.
    - Modified constructor to take iconData, title, and subTitle directly.
    - Added an Expanded widget for better layout adaptability.
    - Adjusted icon size to 24 for consistency.

--- lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_user_job_details_item.dart
+++ lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_user_job_details_item.dart
@@ -2,54 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
 import 'package:road_man_project/core/utilities/base_text_styles.dart';
 
-import '../../../../data/model/job_details_user_job_details_item_model.dart';
-
 class JobDetailsUserJobDetailsItem extends StatelessWidget {
   const JobDetailsUserJobDetailsItem({
     super.key,
-    required this.jobDetailsUserJobDetailsItemModel,
+    required this.iconData,
+    required this.title,
+    required this.subTitle,
   });
-  final JobDetailsUserJobDetailsItemModel jobDetailsUserJobDetailsItemModel;
+  final IconData iconData;
+  final String title, subTitle;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color(0xffE6E8EE),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        spacing: 8,
-        children: [
-          Container(
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: kSecondlyLightWhiteColor,
-              shape: BoxShape.circle,
-            ),
-            child: Center(
-              child: Icon(
-                jobDetailsUserJobDetailsItemModel.iconData,
-                color: kAppPrimaryBlueColor,
-                size: jobDetailsUserJobDetailsItemModel.iconSize,
+    return Expanded(
+      child: Container(
+        height: 96,
+        decoration: BoxDecoration(
+          color: Color(0xffE6E8EE),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 8,
+          children: [
+            Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: kSecondlyLightWhiteColor,
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Icon(
+                  //jobDetailsUserJobDetailsItemModel.iconData,
+                  iconData,
+                  color: kAppPrimaryBlueColor,
+                  //size: jobDetailsUserJobDetailsItemModel.iconSize,
+                  size: 24,
+                ),
               ),
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            spacing: 6,
-            children: [
-              Text(
-                jobDetailsUserJobDetailsItemModel.title,
-                style: AfacadTextStyles.textStyle14W400Black,
-              ),
-              Text(
-                jobDetailsUserJobDetailsItemModel.subTitle,
-                style: AfacadTextStyles.textStyle14W400Blue,
-              ),
-            ],
-          ),
-        ],
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 6,
+              children: [
+                Text(title, style: AfacadTextStyles.textStyle14W400Black),
+                Text(subTitle, style: AfacadTextStyles.textStyle14W400Blue),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
