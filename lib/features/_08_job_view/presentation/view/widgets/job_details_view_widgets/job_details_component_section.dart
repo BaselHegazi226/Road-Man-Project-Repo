6a50612db05ccd042ac4689e18c85cb0945a1c32
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 13:23:56 +0200

    refactor: make JobDetailsComponentSection dynamic
    - Added JobViewCardModel parameter for dynamic job details.
    - Updated JobDetailsTitleLocationSection to receive data from the model.
    - Passed job details model to JobDetailsUserJobDetailsItemsSection.
    - Ensured consistency with previous refactoring.

--- lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_component_section.dart
+++ lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_component_section.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_08_job_view/data/model/job_view_card_model.dart';
 import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_description.dart';
 import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_user_job_details_items_section.dart';
 
 import 'job_details_title_location_section.dart';
 
 class JobDetailsComponentSection extends StatelessWidget {
-  const JobDetailsComponentSection({super.key});
-
+  const JobDetailsComponentSection({
+    super.key,
+    required this.jobDetailsCardModel,
+  });
+  final JobViewCardModel jobDetailsCardModel;
   @override
   Widget build(BuildContext context) {
     return Column(
       spacing: 16,
       children: [
-        JobDetailsTitleLocationSection(),
+        JobDetailsTitleLocationSection(
+          title: jobDetailsCardModel.title,
+          company: '${jobDetailsCardModel.company}|',
+          location: jobDetailsCardModel.location,
+        ),
         ConstrainedBox(
           constraints: BoxConstraints(
             maxHeight: MediaQuery.of(context).size.height * 0.28,
           ),
-          child: JobDetailsUserJobDetailsItemsSection(),
+          child: JobDetailsUserJobDetailsItemsSection(
+            jobViewCardModel: jobDetailsCardModel,
+          ),
         ),
         Padding(
           padding: const EdgeInsets.only(top: 8),
