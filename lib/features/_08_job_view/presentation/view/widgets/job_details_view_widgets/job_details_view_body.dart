302dfbaa3b5255229d9951a95e82f4a816c10d12
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 13:20:34 +0200

    feat: pass jobDetailsCardModel to JobDetailsViewBody
    - Added jobDetailsCardModel as a required parameter in JobDetailsViewBody.
    - Passed jobDetailsCardModel to JobDetailsComponentSection for better data management.

--- lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_view_body.dart
+++ lib/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_view_body.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
 import 'package:road_man_project/core/utilities/custom_flexible_widget.dart';
+import 'package:road_man_project/features/_08_job_view/data/model/job_view_card_model.dart';
 import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_component_section.dart';
 
 import 'job_details_goole_icon.dart';
 
 class JobDetailsViewBody extends StatelessWidget {
-  const JobDetailsViewBody({super.key});
-
+  const JobDetailsViewBody({super.key, required this.jobDetailsCardModel});
+  final JobViewCardModel jobDetailsCardModel;
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -32,7 +33,9 @@ class JobDetailsViewBody extends StatelessWidget {
                 right: 0,
                 left: 0,
                 child: CustomFlexibleWidget(
-                  child: JobDetailsComponentSection(),
+                  child: JobDetailsComponentSection(
+                    jobDetailsCardModel: jobDetailsCardModel,
+                  ),
                 ),
               ),
             ],
