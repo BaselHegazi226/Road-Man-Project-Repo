5c56637163279f0a099ec3494b58ff91898de3f6
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 17:16:40 +0200

    - Made the height dynamic using `screenHeight * 0.05` instead of a fixed value. - Adjusted the border radius dynamically using `screenWidth * 0.02` instead of a fixed `8` pixels.

--- lib/features/_08_job_view/presentation/view/widgets/recent_jobs_widgets/recent_jobs_item.dart
+++ lib/features/_08_job_view/presentation/view/widgets/recent_jobs_widgets/recent_jobs_item.dart
@@ -6,20 +6,27 @@ import '../../../../../../core/helper/const_variables.dart';
 class RecentJobsItem extends StatefulWidget {
   const RecentJobsItem({super.key, required this.text});
   final String text;
+
   @override
   State<RecentJobsItem> createState() => _RecentJobsItemState();
 }
 
 class _RecentJobsItemState extends State<RecentJobsItem> {
-  Color backgroundColor = Color(0xffE6E8EE);
+  Color backgroundColor = const Color(0xffE6E8EE);
   Color textColor = kTextBlackPrimaryColor;
   bool isActiveButton = false;
+
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Container(
-      height: 40,
+      height: screenHeight * 0.05, // ارتفاع متناسب مع الشاشة
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
+        borderRadius: BorderRadius.circular(
+          screenWidth * 0.02,
+        ), // تناسب مع العرض
         color: !isActiveButton ? backgroundColor : kAppPrimaryBlueColor,
       ),
       child: TextButton(
