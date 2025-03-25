7b5c7356e472fbabb58bec6d030d082b023e6c77
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 13:21:11 +0200

    feat: pass job details via GoRouter to JobDetailsView
    - Retrieved JobViewCardModel from GoRouterState in JobDetailsView.
    - Passed job details to JobDetailsViewBody for improved data flow.
    - Added necessary imports for GoRouter support.

--- lib/features/_08_job_view/presentation/view/job_details_view.dart
+++ lib/features/_08_job_view/presentation/view/job_details_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
+import 'package:road_man_project/features/_08_job_view/data/model/job_view_card_model.dart';
 import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_app_bar.dart';
 import 'package:road_man_project/features/_08_job_view/presentation/view/widgets/job_details_view_widgets/job_details_view_body.dart';
 
@@ -8,12 +10,14 @@ class JobDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final jobViewCardModel =
+        GoRouterState.of(context).extra as JobViewCardModel;
     return Scaffold(
       backgroundColor: kAppPrimaryBlueColor,
       appBar: jobDetailsAppbar(context),
       body: Padding(
-        padding: EdgeInsets.only(top: 64),
-        child: const JobDetailsViewBody(),
+        padding: const EdgeInsets.only(top: 64),
+        child: JobDetailsViewBody(jobDetailsCardModel: jobViewCardModel),
       ),
     );
   }
