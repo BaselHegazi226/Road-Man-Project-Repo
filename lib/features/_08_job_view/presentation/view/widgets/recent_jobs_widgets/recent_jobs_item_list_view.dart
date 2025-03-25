d2143cdc001f56ae9bd435fa73498577662b8799
baselhegazi <baselmohammed226@gmail.com>
2025-03-25 17:15:38 +0200

    - Made the height dynamic using `screenHeight * 0.043` instead of a fixed value. - Adjusted the right padding dynamically using `screenWidth * 0.04` instead of a fixed `16` pixels.

--- lib/features/_08_job_view/presentation/view/widgets/recent_jobs_widgets/recent_jobs_item_list_view.dart
+++ lib/features/_08_job_view/presentation/view/widgets/recent_jobs_widgets/recent_jobs_item_list_view.dart
@@ -3,23 +3,28 @@ import 'package:road_man_project/features/_08_job_view/presentation/view/widgets
 
 class RecentJobsItemListView extends StatelessWidget {
   const RecentJobsItemListView({super.key});
+
   static const List<String> textList = [
     'All',
     'Design',
     'Development',
     'Marketing & Content Creator',
   ];
+
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return SizedBox(
-      height: 40,
+      height: screenHeight * 0.043, // ارتفاع متناسب مع حجم الشاشة
       child: ListView.builder(
         itemCount: textList.length,
         scrollDirection: Axis.horizontal,
         physics: const BouncingScrollPhysics(),
         itemBuilder: (context, index) {
           return Padding(
-            padding: const EdgeInsets.only(right: 16),
+            padding: EdgeInsets.only(right: screenWidth * 0.04), // تباعد نسبي
             child: RecentJobsItem(text: textList[index]),
           );
         },
