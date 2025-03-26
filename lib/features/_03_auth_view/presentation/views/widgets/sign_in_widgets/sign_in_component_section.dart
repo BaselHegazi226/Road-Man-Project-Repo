891ef8f91d17615e5d50ea867be4efc435bb1125
baselhegazi <baselmohammed226@gmail.com>
2025-03-26 15:24:49 +0200

    Refactor SignInComponentSection:
    - Made padding and spacing responsive using `MediaQuery` for better adaptability to different screen sizes.
    - Replaced fixed pixel values with proportional calculations based on `screenWidth` and `screenHeight`.
    - Improved UI scalability and consistency across various devices.

--- lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_component_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_component_section.dart
@@ -10,21 +10,29 @@ class SignInComponentSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return AuthComponentSection(
       title: 'Login to your account',
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+        padding: EdgeInsets.symmetric(
+          horizontal: screenWidth * 0.04,
+        ), // بدل 16px
         child: Column(
           children: [
             Padding(
-              padding: EdgeInsets.only(top: 24, bottom: 12),
+              padding: EdgeInsets.only(
+                top: screenHeight * 0.03, // بدل 24px
+                bottom: screenHeight * 0.015, // بدل 12px
+              ),
               child: SocialAuthCardSection(
                 onPressed1: () {},
                 onPressed2: () {},
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(bottom: 24),
+              padding: EdgeInsets.only(bottom: screenHeight * 0.03), // بدل 24px
               child: const AuthSubTitle(subTitle: 'Or use your email account'),
             ),
             SignInInputDataSection(),
