30ecc0cb334cda970b6c153caec5a51f810394d9
baselhegazi <baselmohammed226@gmail.com>
2025-03-26 15:22:56 +0200

    Refactor RememberMeCard:
    - Made the widget responsive by using `MediaQuery.of(context).size.width` to calculate dimensions dynamically.
    - Adjusted width, height, border radius, and border width as a percentage of screen width for better scalability across devices.
    - Updated the icon size to be proportional to screen width for consistent UI appearance.
    - Ensured the widget maintains a square aspect ratio while adapting to different screen sizes.

--- lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_remember_me_card.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_remember_me_card.dart
@@ -6,20 +6,28 @@ class RememberMeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
-      width: 36,
-      height: 36,
+      width: screenWidth * 0.09, // نسبة من عرض الشاشة
+      height: screenWidth * 0.09, // الحفاظ على الشكل المربع بنفس النسبة
       decoration: BoxDecoration(
         color: Colors.transparent,
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: kSecondlyDarkWhiteColor, width: 4),
+        borderRadius: BorderRadius.circular(
+          screenWidth * 0.03,
+        ), // نسبة من العرض
+        border: Border.all(
+          color: kSecondlyDarkWhiteColor,
+          width: screenWidth * 0.01, // نسبة من العرض
+        ),
       ),
       child: Center(
         child: IconButton(
           icon: Icon(
             Icons.check_box_outline_blank_rounded,
             color: Colors.transparent,
-            size: 28,
+            size: screenWidth * 0.07, // نسبة من العرض
           ),
           onPressed: () {},
           padding: EdgeInsets.zero,
