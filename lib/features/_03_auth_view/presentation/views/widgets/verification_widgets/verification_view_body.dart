8d56318e597648ea440b9d28d14296839ed98ee2
baselhegazi <baselmohammed226@gmail.com>
2025-03-26 17:49:16 +0200

    Changes: - Introduced `MediaQuery` to get `screenHeight` and `screenWidth` for dynamic responsiveness. - Replaced fixed horizontal padding (`16`) with `screenWidth * 0.04` for adaptive layout. - Adjusted `heightFactor` of `ClipRect` from `.7` to `screenHeight * .00085` for better scaling. - Modified `top` padding of `CustomAuthText` from `58` to `screenHeight * 0.07` for dynamic spacing. - Fixed spacing issue in the verification message text.

--- lib/features/_03_auth_view/presentation/views/widgets/verification_widgets/verification_view_body.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/verification_widgets/verification_view_body.dart
@@ -10,23 +10,30 @@ class VerificationViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+      padding: EdgeInsets.symmetric(
+        horizontal: screenWidth * 0.04,
+      ), // 5% من العرض بدل 16 بكسل
       child: Column(
-        spacing: 24,
         children: [
           ClipRect(
             child: Align(
               alignment: Alignment.topCenter,
-              heightFactor: .7,
+              heightFactor:
+                  screenHeight * .00085, // يمكنك ضبطه بناءً على اختبارك
               child: CustomImage(image: Assets.authVerificationImage),
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(top: 58),
+            padding: EdgeInsets.only(
+              top: screenHeight * 0.07,
+            ), // 7% من الطول بدل 58 بكسل
             child: CustomAuthText(
               text:
-                  'We have sent a verification code to your email or phone number.             Enter the code below to complete the process',
+                  'We have sent a verification code to your email or phone number. Enter the code below to complete the process.',
               textAlign: TextAlign.center,
             ),
           ),
