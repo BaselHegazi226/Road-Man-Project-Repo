e64cb3da37e21073452da0290fc12fcccbf28574
baselhegazi <baselmohammed226@gmail.com>
2025-03-26 18:06:23 +0200

    update custom_app_bar file path

--- lib/features/_06_profile_view/presentation/view/about_app_view.dart
+++ lib/features/_06_profile_view/presentation/view/about_app_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:road_man_project/core/helper/class_const_functions.dart';
 import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/03_about_app_widgets/about_app_view_body.dart';
 
 import '../../../../core/helper/const_variables.dart';
+import '../../../_03_auth_view/presentation/views/widgets/common_widgets/custom_app_bar.dart';
 
 class AboutAppView extends StatelessWidget {
   const AboutAppView({super.key});
@@ -12,7 +12,7 @@ class AboutAppView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kAppPrimaryWhiteColor,
-      appBar: ConstFunctions.customAppBar(
+      appBar: customAppBar(
         backgroundColor: kAppPrimaryWhiteColor,
         generalColor: kTextBlackPrimaryColor,
         text: 'About App',
