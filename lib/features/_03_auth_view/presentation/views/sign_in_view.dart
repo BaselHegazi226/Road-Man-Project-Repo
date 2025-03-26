760d43b83016be49ebf07885fc2fa25e54283df3
baselhegazi <baselmohammed226@gmail.com>
2025-03-26 15:16:56 +0200

    Mark `SignInViewBody` as constant in `SignInView`
    - Added `const` keyword to `SignInViewBody` inside `CustomScaffoldWidget`.
    - Optimized widget tree by reducing unnecessary rebuilds.

--- lib/features/_03_auth_view/presentation/views/sign_in_view.dart
+++ lib/features/_03_auth_view/presentation/views/sign_in_view.dart
@@ -7,6 +7,6 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffoldWidget(body: SignInViewBody());
+    return const CustomScaffoldWidget(body: SignInViewBody());
   }
 }
