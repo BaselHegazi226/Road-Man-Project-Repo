d41866cc1bb9aaca2f051786c47064eac537f407
baselhegazi <baselmohammed226@gmail.com>
2025-04-13 06:46:47 +0200

    feat(onboarding): update OnBoardingViewImageSection for responsive image display
    The widget now displays onboarding images responsively with a flexible height based on the screen size.

---
+++ lib/features/_02_onboarding_view/presentation/view/widgets/onboarding_view_widgets/onboarding_view_image_section.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class OnBoardingViewImageSection extends StatelessWidget {
+  const OnBoardingViewImageSection({super.key, required this.image});
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.sizeOf(context).height;
+    return Padding(
+      padding: EdgeInsets.only(
+        top: screenHeight * .055,
+        bottom: screenHeight * .03,
+      ),
+      child: Center(
+        child: Flexible(
+          child: SvgPicture.asset(
+            image,
+            height: screenHeight * .48,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
