54b3c828f218dd9012c6c9034eaad7d367a280ea
baselhegazi <baselmohammed226@gmail.com>
2025-04-15 04:53:17 +0200

    add questionnaire_view widgets

---
+++ lib/features/_04_questionnaire_view/presentation/views/widgets/option_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utilities/shadows.dart';
+
+class OptionButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const OptionButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: const [
+          BoxShadows.blackShadow,
+        ],
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 14,
+            color: Colors.black
+          ),
+        ),
+      ),
+    );
+  }
+}
