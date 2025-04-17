2ffcc00728f279de729988e81f193297cd1bcab5
baselhegazi <baselmohammed226@gmail.com>
2025-04-17 17:53:32 +0200

    create quiz,lesson view

---
+++ lib/features/_07_learn_view/presentation/view/widgets/lesson_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LessonView extends StatelessWidget {
+  const LessonView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
