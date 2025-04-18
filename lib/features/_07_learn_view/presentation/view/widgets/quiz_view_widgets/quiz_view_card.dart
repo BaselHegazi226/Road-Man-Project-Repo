e1720d197254a896f9f18731bb261cd5664c80cb
baselhegazi <baselmohammed226@gmail.com>
2025-04-18 21:58:09 +0200

    🎮 Created QuizViewCard: Styled container for quiz question and answers
    - Designed a responsive container with max height and padding.
    - Added a shadow effect for depth and visual appeal.
    - Integrated a scrollable view for displaying question and answers using QuizViewCardQuestionAnswers.
    - Ensured UI adjusts to different screen sizes with MediaQuery.
    
    ✅ Ready for functional testing and UI refinement.

---
+++ lib/features/_07_learn_view/presentation/view/widgets/quiz_view_widgets/quiz_view_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_07_learn_view/data/model/quiz_view_card_model.dart';
+import 'package:road_man_project/features/_07_learn_view/presentation/view/widgets/quiz_view_widgets/quiz_view_card_question_answers.dart';
+
+import '../../../../../../core/helper/const_variables.dart';
+
+class QuizViewCard extends StatelessWidget {
+  const QuizViewCard({
+    super.key,
+    required this.quizViewCardModel,
+    required this.screenHeight,
+    required this.screenWidth,
+  });
+
+  final double screenHeight, screenWidth;
+  final QuizViewCardModel quizViewCardModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        constraints: BoxConstraints(
+          // أقصى ارتفاع يسمح بعرضه قبل ما يبدأ Scroll
+          maxHeight: screenHeight * 0.22,
+          minHeight: 0,
+        ),
+        width: screenWidth,
+        padding: EdgeInsets.symmetric(
+          horizontal: screenWidth * 0.02,
+          vertical: screenHeight * 0.01,
+        ),
+        decoration: BoxDecoration(
+          color: kQuizViewWhiteColor,
+          borderRadius: BorderRadius.circular(screenWidth * 0.02),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, 4),
+              spreadRadius: 0,
+              blurRadius: 4,
+              color: const Color.fromRGBO(0, 0, 0, 0.25),
+            ),
+          ],
+        ),
+        child: SingleChildScrollView(
+          child: QuizViewCardQuestionAnswers(
+            question: quizViewCardModel.questionText,
+            answers: quizViewCardModel.answers,
+          ),
+        ),
+      ),
+    );
+  }
+}
