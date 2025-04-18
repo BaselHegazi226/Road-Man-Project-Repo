d352f628b98bbb6a1c163f2e2af445da507e3faf
baselhegazi <baselmohammed226@gmail.com>
2025-04-19 00:10:17 +0200

    Refactor `ProfileView` to Add Multiple Variants and Navigation
    - Introduced two new variants of the `ProfileView` widget: `ProfileView1` and `ProfileView2`.
    - In `ProfileView1`, added navigation functionality to go back using `GoRouter` when the app bar button is pressed.
    - In `ProfileView2`, replaced the custom app bar with a default `AppBar` and customized its appearance with a centered title and new text style.
    - Updated the `ProfileView` widget to retain the previous structure with a custom app bar.

--- lib/features/_06_profile_view/presentation/view/profile_view.dart
+++ lib/features/_06_profile_view/presentation/view/profile_view.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
 import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/00_profile_widgets/profile_view_body.dart';
 
+import '../../../../core/utilities/base_text_styles.dart';
 import '../../../../core/utilities/custom_app_bar.dart';
 
-class ProfileView extends StatelessWidget {
-  const ProfileView({super.key});
+class ProfileView1 extends StatelessWidget {
+  const ProfileView1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kAppPrimaryWhiteColor,
-      appBar: customAppBar(text: 'Profile', onPressed: () {}),
+      appBar: customAppBar(
+        text: 'Profile',
+        onPressed: () {
+          GoRouter.of(context).pop();
+        },
+      ),
+      body: const ProfileViewBody(),
+    );
+  }
+}
+
+class ProfileView2 extends StatelessWidget {
+  const ProfileView2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kAppPrimaryWhiteColor,
+      appBar: AppBar(
+        backgroundColor: kAppPrimaryWhiteColor,
+        centerTitle: true,
+        title: Text('Profile', style: AfacadTextStyles.textStyle20W600Black),
+      ),
       body: const ProfileViewBody(),
     );
   }
