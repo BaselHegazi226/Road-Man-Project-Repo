b6aef1c65da3237e3211a07e8b8df6ec88de000d
baselhegazi <baselmohammed226@gmail.com>
2025-04-21 14:38:57 +0200

    remove LearningPreferencesSections

--- lib/features/_06_profile_view/presentation/view/widgets/02_settings_widgets/settings_view_body.dart
+++ lib/features/_06_profile_view/presentation/view/widgets/02_settings_widgets/settings_view_body.dart
@@ -3,7 +3,6 @@ import 'package:road_man_project/core/helper/const_variables.dart';
 import 'package:road_man_project/core/utilities/custom_flexible_widget.dart';
 import 'package:road_man_project/core/utilities/custom_text_button.dart';
 import 'package:road_man_project/core/utilities/custom_title.dart';
-import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/02_settings_widgets/learning_preferences_sections.dart';
 import 'package:road_man_project/features/_06_profile_view/presentation/view/widgets/02_settings_widgets/notification_section.dart';
 
 import 'customize_notification_section.dart';
@@ -26,7 +25,7 @@ class SettingsViewBody extends StatelessWidget {
           spacing: screenHeight * .03,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const LearningPreferencesSections(),
+            // const LearningPreferencesSections(),
             const NotificationSection(),
             const CustomizeNotificationSection(),
             Padding(
