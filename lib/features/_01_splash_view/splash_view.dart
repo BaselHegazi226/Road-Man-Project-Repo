b9ab80636ea22b3db01e9f8e71f5906a6727f1b4
Mohamed Dwedar <dwedar4444@gmail.com>
2025-04-26 01:05:22 +0300

    refactor recommended jobs code

---
+++ lib/features/_01_splash_view/splash_view.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      GoRouter.of(context).pushReplacementNamed('/');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/splash/splash.gif',
+        ),
+      ),
+    );
+  }
+}
