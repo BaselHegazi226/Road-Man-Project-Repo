55063f54ad36c3a4915c08bd8ec17dbd26865a01
baselhegazi <baselmohammed226@gmail.com>
2025-04-29 14:47:13 +0300

    Refactor SignInView: Integrate BlocProvider for AuthBloc
    - Wrapped the `SignInView` widget with a `BlocProvider` to provide `AuthBloc` to the widget tree.
    - Created an instance of `AuthBloc` using `AuthRepoImplement` as the authentication repository.
    - Ensured `SignInView` is now dependent on the `AuthBloc` for managing sign-in state and business logic.
    - Kept the `CustomScaffoldWidget` and `SignInViewBody` unchanged, but now they have access to the authentication state through the `AuthBloc`.

--- lib/features/_03_auth_view/presentation/views/sign_in_view.dart
+++ lib/features/_03_auth_view/presentation/views/sign_in_view.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:road_man_project/core/utilities/custom_scaffold_widget.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_view_body.dart';
+
+import '../../data/repos/auth_repo_implement.dart';
+import '../view_model/auth_bloc/auth_bloc.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScaffoldWidget(body: SignInViewBody());
+    return BlocProvider(
+      create: (context) {
+        return AuthBloc(authRepo: AuthRepoImplement());
+      },
+      child: const CustomScaffoldWidget(body: SignInViewBody()),
+    );
   }
 }
