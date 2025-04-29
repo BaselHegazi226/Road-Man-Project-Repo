94e3e0f76123f1af3187eaf7455bc14503e4df1c
baselhegazi <baselmohammed226@gmail.com>
2025-04-29 14:44:40 +0300

    feat: Add BlocProvider and AuthBloc to SignUpView for Authentication
    - Integrated `AuthBloc` using `BlocProvider` to manage authentication states and events in `SignUpView`.
    - Injected `AuthRepoImplement` to handle authentication operations (sign up and sign in).
    - Refactored `SignUpView` to dynamically provide `AuthBloc` for the widget tree.

--- lib/features/_03_auth_view/presentation/views/sign_up_view.dart
+++ lib/features/_03_auth_view/presentation/views/sign_up_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:road_man_project/core/utilities/custom_scaffold_widget.dart';
+import 'package:road_man_project/features/_03_auth_view/data/repos/auth_repo_implement.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_bloc.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_up_widgets/sign_up_view_body.dart';
 
 class SignUpView extends StatelessWidget {
@@ -7,6 +10,11 @@ class SignUpView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScaffoldWidget(body: SignUpViewBody());
+    return BlocProvider(
+      create: (context) {
+        return AuthBloc(authRepo: AuthRepoImplement());
+      },
+      child: const CustomScaffoldWidget(body: SignUpViewBody()),
+    );
   }
 }
