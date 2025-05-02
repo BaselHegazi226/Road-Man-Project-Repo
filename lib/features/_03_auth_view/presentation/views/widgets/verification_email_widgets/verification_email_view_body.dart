44f4f59692a776fb43ca538939f2d3830fa2eb6c
baselhegazi <baselmohammed226@gmail.com>
2025-05-02 15:17:54 +0300

    ✨ Add `VerificationEmailViewBody` widget: - Displays the email verification body view with a custom image and descriptive text. - Includes an input section for the user to enter the verification code. - Uses `CustomImage` to show a verification-related image. - Utilizes `CustomAuthText` for presenting a clear verification instruction message. - Responsive layout using padding based on screen height and width. - Passes the user's email to `VerificationEmailInputDataSection` for verification processing.

---
+++ lib/features/_03_auth_view/presentation/views/widgets/verification_email_widgets/verification_email_view_body.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/common_widgets/custom_auth_text.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/verification_email_widgets/verification_email_input_data_section.dart';
+import 'package:road_man_project/generated/assets.dart';
+
+import '../common_widgets/custom_image.dart';
+
+class VerificationEmailViewBody extends StatelessWidget {
+  const VerificationEmailViewBody({super.key, required this.email});
+  final String email;
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: screenWidth * 0.04,
+      ), // 5% من العرض بدل 16 بكسل
+      child: Column(
+        children: [
+          ClipRect(
+            child: Align(
+              alignment: Alignment.topCenter,
+              heightFactor: screenHeight * .00085,
+              child: CustomImage(image: Assets.authVerificationImage),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: screenHeight * 0.07,
+            ), // 7% من الطول بدل 58 بكسل
+            child: CustomAuthText(
+              text:
+                  'We have sent a verification code to your email or phone number. Enter the code below to complete the process.',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          VerificationEmailInputDataSection(email: email),
+        ],
+      ),
+    );
+  }
+}
