b5c98ee944f7baf3ee724aa2942788fa0ae4b276
baselhegazi <baselmohammed226@gmail.com>
2025-05-02 15:56:26 +0300

    Refactor AuthBloc to Include Additional Auth Events and State Management
    - Added new event handlers for `ForgetPasswordEvent`, `VerifyOtpEvent`, `VerifyEmailEvent`, and `ResetPasswordEvent`.
    - Updated state management to handle additional loading, success, and failure states for the new events.
    - Improved naming consistency by updating `AuthState` to `AuthStates` in the `AuthBloc` class.
    - Ensured error handling for each new event, returning appropriate failure messages when needed.
    - Updated the bloc to reflect a more comprehensive authentication process, including password reset, email verification, and OTP verification.
    
    Changes Summary:
    - **Before:** The `AuthBloc` handled only SignUp and SignIn events.
    - **After:** The `AuthBloc` now also handles ForgetPassword, VerifyOtp, VerifyEmail, and ResetPassword events, with additional states for each.
    
    This refactor enhances the flexibility and robustness of the authentication process in the app.

--- lib/features/_03_auth_view/presentation/view_model/auth_bloc/auth_bloc.dart
+++ lib/features/_03_auth_view/presentation/view_model/auth_bloc/auth_bloc.dart
@@ -4,18 +4,22 @@ import 'package:road_man_project/features/_03_auth_view/data/repos/auth_repo.dar
 import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_event.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_state.dart';
 
-class AuthBloc extends Bloc<AuthEvent, AuthState> {
+class AuthBloc extends Bloc<AuthEvent, AuthStates> {
   final AuthRepo authRepo;
 
   AuthBloc({required this.authRepo}) : super(AuthInitial()) {
     // Register event handlers here
     on<SignUpEvent>(_onSignUpEvent);
     on<SignInEvent>(_onSignInEvent);
+    on<ForgetPasswordEvent>(_onForgetPasswordEvent);
+    on<VerifyOtpEvent>(_onVerifyOtpEvent);
+    on<VerifyEmailEvent>(_onVerifyEmailEvent);
+    on<ResetPasswordEvent>(_onResetPasswordEvent);
   }
-
+  //Sign up Event
   Future<void> _onSignUpEvent(
     SignUpEvent event,
-    Emitter<AuthState> emit,
+    Emitter<AuthStates> emit,
   ) async {
     emit(SignUpLoading());
     var result = await authRepo.signUp(
@@ -25,7 +29,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     );
     result.fold(
       (error) {
-        print('${error.errorMessage}');
         return emit(
           SignUpFailure(errorMessage: error.errorMessage ?? 'bloc error'),
         );
@@ -42,25 +45,129 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     );
   }
 
+  //Sign in Event
   Future<void> _onSignInEvent(
     SignInEvent event,
-    Emitter<AuthState> emit,
+    Emitter<AuthStates> emit,
   ) async {
-    emit(SignUpLoading());
+    emit(SignInLoading());
     var result = await authRepo.signIn(
       email: event.email,
       password: event.password,
     );
     result.fold(
       (error) {
-        print('${error.errorMessage}');
         return emit(
-          SignUpFailure(errorMessage: error.errorMessage ?? 'bloc error'),
+          SignInFailure(errorMessage: error.errorMessage ?? 'bloc error'),
         );
       },
       (success) {
         return emit(
           SignInSuccess(email: event.email, password: event.password),
+        );
+      },
+    );
+  }
+
+  //Forget Password Event
+  Future<void> _onForgetPasswordEvent(
+    ForgetPasswordEvent event,
+    Emitter<AuthStates> emit,
+  ) async {
+    emit(ForgetPasswordLoading());
+    var result = await authRepo.forgetPassword(email: event.email);
+    result.fold(
+      (error) {
+        return emit(
+          ForgetPasswordFailure(
+            errorMessage: error.errorMessage ?? 'bloc error',
+          ),
+        );
+      },
+      (success) {
+        return emit(ForgetPasswordSuccess(email: event.email));
+      },
+    );
+  }
+
+  // Verify Otp Event
+  Future<void> _onVerifyOtpEvent(
+    VerifyOtpEvent event,
+    Emitter<AuthStates> emit,
+  ) async {
+    emit(VerificationOtpLoading());
+    var result = await authRepo.verificationOtp(
+      email: event.email,
+      otp: event.otp,
+    );
+    result.fold(
+      (error) {
+        return emit(
+          VerificationOtpFailure(
+            errorMessage: error.errorMessage ?? 'bloc error',
+          ),
+        );
+      },
+      (success) {
+        return emit(VerificationOtpSuccess(email: event.email, otp: event.otp));
+      },
+    );
+  }
+
+  //VerifyEmail Event
+  Future<void> _onVerifyEmailEvent(
+    VerifyEmailEvent event,
+    Emitter<AuthStates> emit,
+  ) async {
+    emit(VerificationEmailLoading());
+    var result = await authRepo.verificationEmail(
+      email: event.email,
+      otp: event.otp,
+    );
+    result.fold(
+      (error) {
+        return emit(
+          VerificationEmailFailure(
+            errorMessage: error.errorMessage ?? 'bloc error',
+          ),
+        );
+      },
+      (success) {
+        return emit(
+          VerificationEmailSuccess(email: event.email, otp: event.otp),
+        );
+      },
+    );
+  }
+
+  //ResetPassword Event
+  Future<void> _onResetPasswordEvent(
+    ResetPasswordEvent event,
+    Emitter<AuthStates> emit,
+  ) async {
+    emit(ResetPasswordLoading());
+    var result = await authRepo.resetPassword(
+      otp: event.otp,
+      email: event.email,
+      newPassword: event.newPassword,
+      confirmPassword: event.confirmPassword,
+    );
+    result.fold(
+      (error) {
+        return emit(
+          ResetPasswordFailure(
+            errorMessage: error.errorMessage ?? 'bloc error',
+          ),
+        );
+      },
+      (success) {
+        return emit(
+          ResetPasswordSuccess(
+            email: event.email,
+            otp: event.otp,
+            newPassword: event.newPassword,
+            confirmPassword: event.confirmPassword,
+          ),
         );
       },
     );
