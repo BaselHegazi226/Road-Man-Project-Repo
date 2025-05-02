c74c17258791962e331484f19a2942a845116554
baselhegazi <baselmohammed226@gmail.com>
2025-05-02 15:26:03 +0300

    Refactor ResetPasswordView Widget
    - Added BlocProvider to manage authentication state within ResetPasswordView.
    - Integrated ResetPasswordViewBody widget to handle the reset password functionality.
    - Modified ResetPasswordView to retrieve and pass email and OTP data via GoRouterState.
    - Replaced static UI with dynamic data handling for email and OTP within the widget.

--- lib/features/_03_auth_view/presentation/views/reset_password_view.dart
+++ lib/features/_03_auth_view/presentation/views/reset_password_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:road_man_project/core/utilities/custom_app_bar.dart';
+import 'package:road_man_project/features/_03_auth_view/data/repos/auth_repo_implement.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_bloc.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/reset_password_widgets/reset_password_view_body.dart';
 
 import '../../../../core/helper/const_variables.dart';
 
@@ -9,6 +13,8 @@ class ResetPasswordView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Map<String, dynamic> emailAndOtp =
+        GoRouterState.of(context).extra as Map<String, dynamic>;
     return Scaffold(
       backgroundColor: kAppPrimaryWhiteColor,
       appBar: customAppBar(
@@ -18,6 +24,10 @@ class ResetPasswordView extends StatelessWidget {
         onPressed: () {
           GoRouter.of(context).pop();
         },
+      ),
+      body: BlocProvider(
+        create: (context) => AuthBloc(authRepo: AuthRepoImplement()),
+        child: ResetPasswordViewBody(emailAndOtp: emailAndOtp),
       ),
     );
   }
