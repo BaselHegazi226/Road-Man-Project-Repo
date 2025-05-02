d960a5192b95a0b419bd360625c29fc18839f893
baselhegazi <baselmohammed226@gmail.com>
2025-05-02 15:29:48 +0300

    Enhance ForgetPasswordInputDataSection with AuthBloc integration for password reset
    - Integrated `AuthBloc` with `ForgetPasswordInputDataSection` for handling password reset state.
    - Replaced manual form submission logic with `BlocConsumer` to manage state transitions (loading, success, failure) more effectively.
    - Added handling for `ForgetPasswordSuccess` and `ForgetPasswordFailure` states to show appropriate dialogs and navigate accordingly.
    - Integrated a `CustomCircleIndicator` to indicate loading during the password reset process.
    - Added `ForgetPasswordEvent` to trigger password reset when form validation is successful.
    - Updated the `CustomTextButton` to disable during loading state and enable after successful state transition.
    - Ensured proper form validation and state handling for the email input field.

--- lib/features/_03_auth_view/presentation/views/widgets/forget_password_widgets/forget_password_input_data_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/forget_password_widgets/forget_password_input_data_section.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:road_man_project/core/utilities/custom_circle_indicator.dart';
+import 'package:road_man_project/core/utilities/dialogState.dart';
+import 'package:road_man_project/core/utilities/routes.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_bloc.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_event.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_state.dart';
 
 import '../../../../../../core/helper/const_variables.dart';
 import '../../../../../../core/utilities/custom_text_button.dart';
@@ -95,12 +103,55 @@ class _ForgetPasswordInputDataSectionState
           ),
           Padding(
             padding: EdgeInsets.only(top: screenHeight * .005),
-            child: CustomTextButton(
-              onPressed: () {
-                if (!_formKey.currentState!.validate()) {}
+            child: BlocConsumer<AuthBloc, AuthState>(
+              listener: (context, state) {
+                if (state is ForgetPasswordSuccess) {
+                  customAwesomeDialog(
+                    context: context,
+                    title: 'Password Reset Email Sent',
+                    description:
+                        'We \'ve sent a password reset link to your email. Please check your inbox and follow the instructions to reset your password.',
+                    isSuccess: true,
+                    onPressed: () {
+                      GoRouter.of(
+                        context,
+                      ).push(Routes.verificationOtpViewId, extra: state.email);
+                    },
+                  );
+                } else if (state is ForgetPasswordFailure) {
+                  customAwesomeDialog(
+                    context: context,
+                    title: state.errorMessage,
+                    description: 'try again later!',
+                    isSuccess: false,
+                    onPressed: () {
+                      GoRouter.of(context).pop();
+                    },
+                  );
+                }
               },
-              backgroundColor: kAppPrimaryBlueColor,
-              child: CustomTitle(title: 'Reset Password'),
+              builder: (context, state) {
+                return CustomTextButton(
+                  onPressed:
+                      state is ForgetPasswordLoading
+                          ? null
+                          : () {
+                            if (!_formKey.currentState!.validate()) {
+                              _formKey.currentState?.save();
+                              context.read<AuthBloc>().add(
+                                ForgetPasswordEvent(
+                                  email: emailEditingController.text.trim(),
+                                ),
+                              );
+                            }
+                          },
+                  backgroundColor: kAppPrimaryBlueColor,
+                  child:
+                      state is ForgetPasswordLoading
+                          ? const CustomCircleIndicator()
+                          : CustomTitle(title: 'Reset Password'),
+                );
+              },
             ),
           ),
         ],
