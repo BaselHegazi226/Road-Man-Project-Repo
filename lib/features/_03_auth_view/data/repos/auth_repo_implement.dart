2093937a0d66f8487fdd95808cde97ab51e7f135
baselhegazi <baselmohammed226@gmail.com>
2025-05-02 15:32:54 +0300

    Enhance AuthRepoImplement with password reset, OTP verification, and email verification
    - **Added `forgetPassword` method**: Handles the process of initiating a password reset by sending an email for password recovery.
    - **Added `verificationOtp` method**: Adds OTP verification functionality after a password reset request.
    - **Added `verificationEmail` method**: Introduces email verification using OTP.
    - **Updated dependencies**: Added new models (`ForgetPasswordModel`, `VerificationCodeModel`, `VerificationEmailModel`) for handling data related to these new functionalities.
    - **No changes to existing signUp and signIn methods**: Retained existing logic for user sign-up and sign-in, along with the existing error handling.
    - **Unimplemented methods**: The `changePassword`, `resetPassword`, and `signOut` methods remain unimplemented for future completion.
    
    These changes expand the authentication functionality, allowing users to request password resets, verify OTP, and confirm email via OTP.

--- lib/features/_03_auth_view/data/repos/auth_repo_implement.dart
+++ lib/features/_03_auth_view/data/repos/auth_repo_implement.dart
@@ -1,11 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:road_man_project/core/error/failure.dart';
+import 'package:road_man_project/features/_03_auth_view/data/model/forget_password_model.dart';
 import 'package:road_man_project/features/_03_auth_view/data/model/sign_in_model.dart';
+import 'package:road_man_project/features/_03_auth_view/data/model/verification_otp_model.dart';
 import 'package:road_man_project/features/_03_auth_view/data/repos/auth_repo.dart';
 
-import '../../../../generated/assets.dart';
 import '../model/sign_up_model.dart';
+import '../model/verify_email_model.dart';
 
 class AuthRepoImplement implements AuthRepo {
   final Dio dio = Dio();
@@ -23,8 +25,6 @@ class AuthRepoImplement implements AuthRepo {
       name: name,
       email: email,
       password: password,
-      image: Assets.googleImage,
-      dateTime: DateTime.now(),
     );
     //
     // print('name from implementation sign up : (${signUpModel.name})');
@@ -91,9 +91,31 @@ class AuthRepoImplement implements AuthRepo {
   }
 
   @override
-  Future<Either<Failure, void>> forgetPassword({required String email}) {
-    // TODO: implement forgetPassword
-    throw UnimplementedError();
+  Future<Either<Failure, void>> forgetPassword({required String email}) async {
+    final forgetPasswordPath =
+        'http://hazemibrahim2319-001-site1.qtempurl.com/api/Accounts/forgot-password';
+
+    //print('email from implementation forget password : (${forgetPasswordModel.email})');
+    final forgetPasswordModel = ForgetPasswordModel(email: email);
+
+    try {
+      final response = await dio.post(
+        forgetPasswordPath,
+        data: forgetPasswordModel.toJson(),
+      );
+      if (response.statusCode == 200) {
+        return right(null);
+      } else {
+        //print('status code : ${response.statusCode.toString()}');
+        return left(ServerFailure(errorMessage: 'Unexpected error occurred.'));
+      }
+    } on DioException catch (dioException) {
+      //print('dio exception : ${dioException.error.toString()}');
+      return left(ServerFailure(errorMessage: dioException.error.toString()));
+    } catch (e) {
+      //print('error : ${e.toString()}');
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
   }
 
   @override
@@ -111,5 +133,69 @@ class AuthRepoImplement implements AuthRepo {
   Future<Either<Failure, void>> signOut() {
     // TODO: implement signOut
     throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, void>> verificationOtp({
+    required String email,
+    required String otp,
+  }) async {
+    final String verifyOtp =
+        'http://hazemibrahim2319-001-site1.qtempurl.com/api/Accounts/verify-otp';
+    final verificationAfterForgetPasswordModel = VerificationCodeModel(
+      email: email,
+      otp: otp,
+    );
+    try {
+      final response = await dio.post(
+        verifyOtp,
+        data: verificationAfterForgetPasswordModel.toJson(),
+      );
+
+      if (response.statusCode == 200) {
+        return right(null);
+      } else {
+        //print('status code : ${response.statusCode.toString()}');
+        return left(ServerFailure(errorMessage: 'Unexpected error occurred.'));
+      }
+    } on DioException catch (dioException) {
+      //print('dio exception : ${dioException.error.toString()}');
+      return left(ServerFailure(errorMessage: dioException.error.toString()));
+    } catch (e) {
+      //print('error : ${e.toString()}');
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> verificationEmail({
+    required String email,
+    required String otp,
+  }) async {
+    final String verifyEmail =
+        'http://hazemibrahim2319-001-site1.qtempurl.com/api/Accounts/verify-email';
+    final verificationEmailModel = VerificationEmailModel(
+      email: email,
+      otp: otp,
+    );
+    try {
+      final response = await dio.post(
+        verifyEmail,
+        data: verificationEmailModel.toJson(),
+      );
+
+      if (response.statusCode == 200) {
+        return right(null);
+      } else {
+        //print('status code : ${response.statusCode.toString()}');
+        return left(ServerFailure(errorMessage: 'Unexpected error occurred.'));
+      }
+    } on DioException catch (dioException) {
+      //print('dio exception : ${dioException.error.toString()}');
+      return left(ServerFailure(errorMessage: dioException.error.toString()));
+    } catch (e) {
+      //print('error : ${e.toString()}');
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
   }
 }
