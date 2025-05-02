17aa8e6cc2b6c76b91ad91598a26dbb43004535e
baselhegazi <baselmohammed226@gmail.com>
2025-05-02 15:31:28 +0300

    Enhance AuthState with additional password reset and verification states
    - **Added Forget Password States**: Introduced `ForgetPasswordInitial`, `ForgetPasswordFailure`, `ForgetPasswordLoading`, and `ForgetPasswordSuccess` to handle forget password functionality.
    - **Added Reset Password States**: Introduced `ResetPasswordInitial`, `ResetPasswordFailure`, `ResetPasswordLoading`, and `ResetPasswordSuccess` to handle password reset scenarios, including email, OTP, and new password details.
    - **Added Verification OTP States**: Introduced `VerificationOtpInitial`, `VerificationOtpFailure`, `VerificationOtpLoading`, and `VerificationOtpSuccess` for handling OTP verification after forget password.
    - **Added Verification Email States**: Introduced `VerificationEmailInitial`, `VerificationEmailFailure`, `VerificationEmailLoading`, and `VerificationEmailSuccess` to handle email verification scenarios for forget password and other actions.
    - **No changes to existing Sign Up and Sign In states**: Retained the existing `SignUp` and `SignIn` states without modification.
    
    These changes introduce additional states for handling password recovery, OTP verification, and email verification, enhancing the flexibility and robustness of the authentication flow.

--- lib/features/_03_auth_view/presentation/view_model/auth_bloc/auth_state.dart
+++ lib/features/_03_auth_view/presentation/view_model/auth_bloc/auth_state.dart
@@ -40,3 +40,72 @@ class SignInSuccess extends AuthState {
 
   SignInSuccess({required this.email, required this.password});
 }
+
+//Forget Password States
+class ForgetPasswordInitial extends AuthState {}
+
+class ForgetPasswordFailure extends AuthState {
+  final String errorMessage;
+  ForgetPasswordFailure({required this.errorMessage});
+}
+
+class ForgetPasswordLoading extends AuthState {}
+
+class ForgetPasswordSuccess extends AuthState {
+  final String email;
+
+  ForgetPasswordSuccess({required this.email});
+}
+
+//Reset Password States
+class ResetPasswordInitial extends AuthState {}
+
+class ResetPasswordFailure extends AuthState {
+  final String errorMessage;
+  ResetPasswordFailure({required this.errorMessage});
+}
+
+class ResetPasswordLoading extends AuthState {}
+
+class ResetPasswordSuccess extends AuthState {
+  final String email, otp, newPassword, confirmPassword;
+
+  ResetPasswordSuccess({
+    required this.email,
+    required this.otp,
+    required this.newPassword,
+    required this.confirmPassword,
+  });
+}
+
+//Verification After Forget Password States
+class VerificationOtpInitial extends AuthState {}
+
+class VerificationOtpFailure extends AuthState {
+  final String errorMessage;
+  VerificationOtpFailure({required this.errorMessage});
+}
+
+class VerificationOtpLoading extends AuthState {}
+
+class VerificationOtpSuccess extends AuthState {
+  final String email, otp;
+
+  VerificationOtpSuccess({required this.email, required this.otp});
+}
+
+//Verification Email States
+class VerificationEmailInitial extends AuthState {}
+
+class VerificationEmailFailure extends AuthState {
+  final String errorMessage;
+  VerificationEmailFailure({required this.errorMessage});
+}
+
+class VerificationEmailLoading extends AuthState {}
+
+class VerificationEmailSuccess extends AuthState {
+  final String email, otp;
+
+  VerificationEmailSuccess({required this.email, required this.otp});
+}
