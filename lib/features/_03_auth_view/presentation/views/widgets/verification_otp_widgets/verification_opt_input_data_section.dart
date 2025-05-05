c080abb7296f5684c21b401a8c476bae36359a12
baselhegazi <baselmohammed226@gmail.com>
2025-05-05 16:03:36 +0300

    refactor(ui): extract OTP action buttons into separate widgets for better modularity
    - Moved resend OTP button logic into `OtpSendAgainButton` widget
    - Moved verify OTP button logic into `VerifyOtpButton` widget
    - Added local loading state management for both buttons using `setState`
    - Improved code readability and separation of concerns

--- lib/features/_03_auth_view/presentation/views/widgets/verification_otp_widgets/verification_opt_input_data_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/verification_otp_widgets/verification_opt_input_data_section.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:road_man_project/core/utilities/base_text_styles.dart';
-import 'package:road_man_project/core/utilities/custom_circle_indicator.dart';
-import 'package:road_man_project/core/utilities/dialogState.dart';
-import 'package:road_man_project/core/utilities/routes.dart';
-import 'package:road_man_project/core/utilities/show_snack_bar.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_bloc.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_event.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_state.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/verification_otp_widgets/verification_otp_fields.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/verification_otp_widgets/verify_otp_button.dart';
 
-import '../../../../../../core/helper/const_variables.dart';
-import '../../../../../../core/utilities/custom_text_button.dart';
-import '../../../../../../core/utilities/custom_title.dart';
+import 'otp_send_again_button.dart';
 
 class VerificationOtpInputDataSection extends StatefulWidget {
   const VerificationOtpInputDataSection({super.key, required this.email});
@@ -28,6 +17,8 @@ class _VerificationOtpInputDataSectionState
     extends State<VerificationOtpInputDataSection> {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  bool sendAgainIsLoading = false;
+  bool buttonIsLoading = false;
 
   final List<TextEditingController> otpControllers = List.generate(
     6,
@@ -62,96 +53,25 @@ class _VerificationOtpInputDataSectionState
             otpFocusNodes: otpFocusNodes,
           ),
         ),
-        BlocConsumer<AuthBloc, AuthStates>(
-          listener: (context, state) {
-            if (state is ForgetPasswordSuccess) {
-              showSnackBar(
-                context,
-                'Please check your inbox',
-                kAppPrimaryBlueColor.withAlpha(125),
-              );
-            } else if (state is ForgetPasswordFailure) {
-              showSnackBar(
-                context,
-                state.errorMessage,
-                kAppPrimaryWrongColor.withAlpha(125),
-              );
-            }
-          },
-          builder: (context, state) {
-            return TextButton(
-              onPressed: () {
-                context.read<AuthBloc>().add(
-                  ForgetPasswordEvent(email: widget.email),
-                );
-              },
-              child: Text(
-                'Send again ?',
-                style: AfacadTextStyles.textStyle20W700Blue(context),
-              ),
-            );
-          },
+
+        OtpSendAgainButton(
+          email: widget.email,
+          isLoading: sendAgainIsLoading,
+          isOtherButtonLoading: buttonIsLoading,
+          setLoading: (val) => setState(() => sendAgainIsLoading = val),
         ),
         Padding(
           padding: EdgeInsets.only(top: screenHeight * .005),
-          child: BlocConsumer<AuthBloc, AuthStates>(
-            listener: (context, state) {
-              if (state is VerificationOtpSuccess) {
-                customAwesomeDialog(
-                  context: context,
-                  isSuccess: true,
-                  title: 'Verification Successful',
-                  description:
-                      'Your OTP has been verified successfully. You can now reset your password',
-                  buttonText: 'Continue to Reset Password',
-                  onPressed: () {
-                    GoRouter.of(context).push(
-                      Routes.resetPasswordViewId,
-                      extra: {
-                        'email': widget.email,
-                        'otp': otpControllers.map((e) => e.text).join(),
-                      },
-                    );
-                  },
-                );
-              } else if (state is VerificationOtpFailure) {
-                customAwesomeDialog(
-                  context: context,
-                  title: 'Verification Failed',
-                  description: state.errorMessage,
-                  buttonText: 'Try Again!',
-                  isSuccess: false,
-                  onPressed: () {
-                    GoRouter.of(context).pop();
-                  },
-                );
-              }
-            },
-            builder: (context, state) {
-              return CustomTextButton(
-                onPressed:
-                    state is VerificationOtpLoading
-                        ? null
-                        : () {
-                          if (_formKey.currentState!.validate()) {
-                            final otp =
-                                otpControllers.map((e) => e.text).join();
-                            context.read<AuthBloc>().add(
-                              VerifyOtpEvent(email: widget.email, otp: otp),
-                            );
-                          } else {
-                            setState(() {
-                              autoValidateMode = AutovalidateMode.always;
-                            });
-                          }
-                        },
-                backgroundColor: kAppPrimaryBlueColor,
-                child:
-                    state is VerificationOtpLoading
-                        ? const CustomCircleIndicator()
-                        : CustomTitle(title: 'Verify Otp'),
-              );
-            },
+          child: VerifyOtpButton(
+            email: widget.email,
+            otpControllers: otpControllers,
+            formKey: _formKey,
+            autoValidateMode: autoValidateMode,
+            setAutoValidateMode:
+                (val) => setState(() => autoValidateMode = val),
+            isLoading: buttonIsLoading,
+            isOtherButtonLoading: sendAgainIsLoading,
+            setLoading: (val) => setState(() => buttonIsLoading = val),
           ),
         ),
       ],
