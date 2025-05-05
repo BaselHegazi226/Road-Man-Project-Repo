84dd398f16be2470f20c064bb5197518d0d6581f
baselhegazi <baselmohammed226@gmail.com>
2025-05-05 16:08:00 +0300

    refactor(auth): use MultiBlocProvider in SignInView
    - Added `SocialAuthBloc` alongside `AuthBloc` to enable social login
    - Prepares `SignInView` for handling Google/LinkedIn sign-ins

--- lib/features/_03_auth_view/presentation/views/sign_in_view.dart
+++ lib/features/_03_auth_view/presentation/views/sign_in_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:road_man_project/core/utilities/custom_scaffold_widget.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/view_model/social_auth_bloc/social_auth_bloc.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_view_body.dart';
 
 import '../../data/repos/auth_repo_implement.dart';
@@ -11,10 +12,19 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) {
-        return AuthBloc(authRepo: AuthRepoImplement());
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return AuthBloc(authRepo: AuthRepoImplement());
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            return SocialAuthBloc(authRepo: AuthRepoImplement());
+          },
+        ),
+      ],
       child: const CustomScaffoldWidget(body: SignInViewBody()),
     );
   }
