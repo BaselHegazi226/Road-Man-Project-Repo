e5efa80cfc1b5ae7b34df8972a7c91a2e07b1176
baselhegazi <baselmohammed226@gmail.com>
2025-05-05 16:08:30 +0300

    refactor(auth): modularize and enhance SignInInputDataSection
    - Moved `BlocConsumer` logic to a separate `SignInBlocConsumer` widget for cleaner separation of concerns.
    - Integrated `SocialAuthCardSection` with loading control via `socialButtonIsLoading`.
    - Disabled navigation actions while social sign-in is loading to prevent conflicts.

--- lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_input_data_section.dart
+++ lib/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_input_data_section.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:road_man_project/core/utilities/custom_circle_indicator.dart';
 import 'package:road_man_project/core/utilities/routes.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_bloc.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_event.dart';
-import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_state.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/common_widgets/social_auth_card_section.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/other_register_section.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_bloc_consumer.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_remember_password_section.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/sign_in_widgets/sign_in_text_form_fields_section.dart';
-
-import '../../../../../../core/helper/const_variables.dart';
-import '../../../../../../core/utilities/custom_text_button.dart';
-import '../../../../../../core/utilities/custom_title.dart';
-import '../../../../../../core/utilities/dialogState.dart';
-import 'other_register_section.dart';
 
 class SignInInputDataSection extends StatefulWidget {
   const SignInInputDataSection({super.key});
@@ -24,108 +16,56 @@ class SignInInputDataSection extends StatefulWidget {
 
 class _SignInInputDataSectionState extends State<SignInInputDataSection> {
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
+  bool socialButtonIsLoading = false;
+
+  bool get anyButtonIsLoading => socialButtonIsLoading;
+
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
+
     return Column(
-      spacing: screenHeight * 0.02,
+      spacing: screenHeight * 0.025,
       children: [
         SignInTextFormFieldsSection(
           formKey: _formKey,
           emailEditingController: emailController,
           passwordEditingController: passwordController,
         ),
+
         RememberMeForgetPasswordSection(
           forgetPasswordPressed: () {
             GoRouter.of(context).push(Routes.forgetPasswordViewId);
           },
         ),
-        Padding(
-          padding: EdgeInsets.only(top: screenHeight * 0.02),
-          child: Column(
-            spacing: screenHeight * 0.02,
-            children: [
-              BlocConsumer<AuthBloc, AuthStates>(
-                listener: (context, state) {
-                  if (state is SignInSuccess) {
-                    customAwesomeDialog(
-                      context: context,
-                      isSuccess: true,
-                      title: 'Sign in Success',
-                      description:
-                          'You have successfully sign into Road Man. Enjoy exploring all the features available to you',
-                      onPressed: () {
-                        GoRouter.of(context).push(Routes.mainViewId);
-                      },
-                    );
-                  } else if (state is SignInSuccess) {
-                    customAwesomeDialog(
-                      context: context,
-                      isSuccess: false,
-                      title: 'Sign In Failure',
-                      description:
-                          'There was an issue with your registration. Please check your details and try again. If the problem persists, contact support',
-                      onPressed: () {
-                        GoRouter.of(context).pop();
-                      },
-                    );
-                  } else {
-                    print('loading sign in');
-                  }
-                },
-                builder: (context, state) {
-                  return CustomTextButton(
-                    onPressed:
-                        state is SignInLoading
-                            ? null
-                            : () {
-                              if (_formKey.currentState?.validate() ?? false) {
-                                _formKey.currentState?.save();
-                                context.read<AuthBloc>().add(
-                                  SignInEvent(
-                                    email: emailController.text.trim(),
-                                    password: passwordController.text.trim(),
-                                  ),
-                                );
-                              }
-                            },
-                    backgroundColor: kAppPrimaryBlueColor,
-                    child:
-                        state is SignInLoading
-                            ? CustomCircleIndicator()
-                            : CustomTitle(
-                              title: 'Sign in',
-                              textColor: kSecondlyLightWhiteColor,
-                            ),
-                  );
-                },
-              ),
-              CustomTextButton(
-                onPressed: () {
-                  GoRouter.of(context).push(Routes.mainViewId);
-                },
-                backgroundColor: kSecondlyLightWhiteColor,
-                borderColor: kAppPrimaryBlueColor,
-                child: CustomTitle(
-                  title: 'Continue as a Guest',
-                  textColor: kAppPrimaryBlueColor,
-                ),
-              ),
-            ],
-          ),
+
+        SignInBlocConsumer(
+          formKey: _formKey,
+          emailController: emailController,
+          passwordController: passwordController,
         ),
-        Padding(
-          padding: EdgeInsets.only(top: screenHeight * 0.005),
-          child: OtherRegisterSection(
-            blackText: 'Don’t have an account? ',
-            blueText: 'Sign up',
-            onTap: () {
-              GoRouter.of(context).push(Routes.signUpViewId);
-            },
-          ),
+
+        SocialAuthCardSection(
+          authButtonIsLoading: anyButtonIsLoading,
+          onSocialAuthLoadingChanged: (isLoading) {
+            setState(() {
+              socialButtonIsLoading = isLoading;
+            });
+          },
+        ),
+
+        OtherRegisterSection(
+          blackText: 'Don’t have an account? ',
+          blueText: 'Sign up',
+          onTap:
+              anyButtonIsLoading
+                  ? null
+                  : () {
+                    GoRouter.of(context).push(Routes.signUpViewId);
+                  },
         ),
       ],
     );
