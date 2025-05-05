b78bccbbd60e06c308f15e52222ef2b00c143a99
baselhegazi <baselmohammed226@gmail.com>
2025-05-05 16:19:45 +0300

    chore(gradle): add Firebase plugin setup scaffold
    - Declared Google Services Gradle plugin for Firebase integration (not yet applied).
    - Added placeholders for Firebase BoM and dependencies for future use.
    - Maintained existing build directory customization and subproject configuration.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,23 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
+plugins {
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.3.15" apply false
+}
+dependencies {
+    // Import the Firebase BoM
+//    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+//    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
