4d693ada8a81b79157097808ab152643936a63af
baselhegazi <baselmohammed226@gmail.com>
2025-05-08 18:11:03 +0300

    Refactor ForgetPasswordView: Wrap ForgetPasswordViewBody with CustomFlexibleWidget
    - Added `CustomFlexibleWidget` to wrap `ForgetPasswordViewBody` for better layout flexibility.
    - The `BlocProvider` now remains at the top level and the body content is wrapped inside the `CustomFlexibleWidget`.
    - This change improves responsiveness and ensures the content adapts dynamically within the available space.

--- lib/features/_03_auth_view/presentation/views/forget_password_view.dart
+++ lib/features/_03_auth_view/presentation/views/forget_password_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:road_man_project/core/helper/const_variables.dart';
 import 'package:road_man_project/core/utilities/custom_app_bar.dart';
+import 'package:road_man_project/core/utilities/custom_flexible_widget.dart';
 import 'package:road_man_project/features/_03_auth_view/data/repos/auth_repo_implement.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/view_model/auth_bloc/auth_bloc.dart';
 import 'package:road_man_project/features/_03_auth_view/presentation/views/widgets/forget_password_widgets/forget_password_view_body.dart';
@@ -12,19 +13,19 @@ class ForgetPasswordView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kAppPrimaryWhiteColor,
-      appBar: customAppBar(
-        context,
-        generalColor: kAppPrimaryBlueColor,
-        text: 'Forget Password',
-        onPressed: () {
-          GoRouter.of(context).pop();
-        },
-      ),
-      body: BlocProvider(
-        create: (context) => AuthBloc(authRepo: AuthRepoImplement()),
-        child: const ForgetPasswordViewBody(),
+    return BlocProvider(
+      create: (context) => AuthBloc(authRepo: AuthRepoImplement()),
+      child: Scaffold(
+        backgroundColor: kAppPrimaryWhiteColor,
+        appBar: customAppBar(
+          context,
+          generalColor: kAppPrimaryBlueColor,
+          text: 'Forget Password',
+          onPressed: () {
+            GoRouter.of(context).pop();
+          },
+        ),
+        body: const CustomFlexibleWidget(child: ForgetPasswordViewBody()),
       ),
     );
   }
