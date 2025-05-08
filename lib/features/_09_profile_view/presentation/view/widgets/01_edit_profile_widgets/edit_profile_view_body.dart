951f4b4cb4bb233f1518f2fee1a210962084398e
baselhegazi <baselmohammed226@gmail.com>
2025-05-08 18:13:20 +0300

    Refactor EditProfileViewBody: Add state management and navigation functionality
    - Converted `EditProfileViewBody` from a `StatelessWidget` to a `StatefulWidget` to handle state for profile name input.
    - Introduced `TextEditingController` for managing the profile name field.
    - Added a `ChangePasswordButton` for navigating to the change password view.
    - Added a custom button (`CustomTextButton`) for updating the profile with a navigation action.
    - Updated the column spacing and widget layout for improved consistency.

--- lib/features/_09_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_view_body.dart
+++ lib/features/_09_profile_view/presentation/view/widgets/01_edit_profile_widgets/edit_profile_view_body.dart
@@ -1,12 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:road_man_project/core/utilities/routes.dart';
 
+import '../../../../../../core/helper/const_variables.dart';
+import '../../../../../../core/utilities/custom_text_button.dart';
+import '../../../../../../core/utilities/custom_title.dart';
+import 'change_password_button.dart';
 import 'edit_profile_image.dart';
 import 'edit_profile_names_fields_section.dart';
-import 'edit_profile_password_fields_section.dart';
 
-class EditProfileViewBody extends StatelessWidget {
+class EditProfileViewBody extends StatefulWidget {
   const EditProfileViewBody({super.key});
 
+  @override
+  State<EditProfileViewBody> createState() => _EditProfileViewBodyState();
+}
+
+class _EditProfileViewBodyState extends State<EditProfileViewBody> {
+  TextEditingController nameEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.sizeOf(context).height;
@@ -17,14 +28,25 @@ class EditProfileViewBody extends StatelessWidget {
         vertical: screenHeight * .04,
       ),
       child: Column(
-        spacing: screenHeight * .02,
+        spacing: screenHeight * .03,
         children: [
           EditProfileImage(
             screenWidth: screenWidth,
             screenHeight: screenHeight,
           ),
-          EditProfileNamesFieldsSection(),
-          EditProfilePasswordFieldsSection(),
+          EditProfileNamesFieldsSection(
+            nameEditingController: nameEditingController,
+          ),
+          ChangePasswordButton(
+            onPressed: () {
+              GoRouter.of(context).push(Routes.changePasswordViewId);
+            },
+          ),
+          CustomTextButton(
+            onPressed: () {},
+            backgroundColor: kAppPrimaryBlueColor,
+            child: CustomTitle(title: 'Update Profile'),
+          ),
         ],
       ),
     );
