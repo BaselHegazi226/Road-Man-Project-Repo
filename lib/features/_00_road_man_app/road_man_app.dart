636076847a2576c65fa6608badfaefdcf758db79
baselhegazi <baselmohammed226@gmail.com>
2025-05-08 18:06:56 +0300

    git commit -m "Wrap app with BlocProvider for RefreshTokenCubit
    - Provided RefreshTokenCubit in the widget tree for managing refresh token functionality"

--- lib/features/_00_road_man_app/road_man_app.dart
+++ lib/features/_00_road_man_app/road_man_app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:road_man_project/features/_03_auth_view/data/repos/auth_repo_implement.dart';
+import 'package:road_man_project/features/_03_auth_view/presentation/view_model/refresh_token_cubit/refresh_token_cubit.dart';
 
 import '../../core/utilities/app_router.dart';
 
@@ -8,9 +11,12 @@ class RoadManApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
+    return BlocProvider(
+      create: (context) => RefreshTokenCubit(authRepo: AuthRepoImplement()),
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
