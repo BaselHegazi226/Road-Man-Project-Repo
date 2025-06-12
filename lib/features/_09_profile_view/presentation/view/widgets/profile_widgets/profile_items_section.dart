e4e33c7c6a79b4c270b0d76b253a0db23751f7dd
baselhegazi <baselmohammed226@gmail.com>
2025-06-12 09:47:35 +0300

    Enhance ProfileItemsSection to refresh user info after editing
    - Added BlocProvider access to call `localGetUserInfo()` after editing profile.
    - Handled route result when returning from EditProfileView.
    - Ensured updated user data is fetched from local storage on successful edit.
    - Improved navigation logic for dynamic route handling.

--- lib/features/_09_profile_view/presentation/view/widgets/profile_widgets/profile_items_section.dart
+++ lib/features/_09_profile_view/presentation/view/widgets/profile_widgets/profile_items_section.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:road_man_project/features/_09_profile_view/presentation/view/widgets/profile_widgets/profile_item.dart';
 
+import '../../../../../../core/utilities/routes.dart';
+import '../../../view_model/get_user_info_cubit/get_user_info_cubit.dart';
 import '../00_profile_common_widgets/profile_const_list.dart';
 
 class ProfileItemsSection extends StatelessWidget {
@@ -11,16 +14,29 @@ class ProfileItemsSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
       itemCount: ProfileConstList.profileItemModelList.length,
       itemBuilder: (context, index) {
         final item = ProfileConstList.profileItemModelList[index];
+
         return ProfileItem(
           profileItemModel: item,
-          onTap:
-              item.routeName.isNotEmpty
-                  ? () => GoRouter.of(context).push(item.routeName)
-                  : null,
+          onTap: () async {
+            // لو الزرار هو Edit Profile
+            if (item.routeName == Routes.editProfileViewId) {
+              final result = await GoRouter.of(
+                context,
+              ).push<bool>(item.routeName);
+
+              if (result == true && context.mounted) {
+                BlocProvider.of<GetUserInfoCubit>(context).localGetUserInfo();
+              }
+            }
+            // باقي العناصر
+            else if (item.routeName.isNotEmpty) {
+              GoRouter.of(context).push(item.routeName);
+            }
+          },
         );
       },
     );
