87f1e5ab19cee94ce2846537c459bbb3dc3c8091
baselhegazi <baselmohammed226@gmail.com>
2025-06-13 19:50:23 +0300

    put the list view in column and add log out item under the reminder items

--- lib/features/_09_profile_view/presentation/view/widgets/profile_widgets/profile_items_section.dart
+++ lib/features/_09_profile_view/presentation/view/widgets/profile_widgets/profile_items_section.dart
@@ -12,6 +12,7 @@ import 'package:road_man_project/features/_09_profile_view/presentation/view_mod
 import '../../../view_model/get_user_info_cubit/get_user_info_cubit.dart';
 import '../../../view_model/profile_blocs/profile_event.dart';
 import '../00_profile_common_widgets/profile_const_list.dart';
+import 'logout_item.dart';
 
 class ProfileItemsSection extends StatefulWidget {
   const ProfileItemsSection({super.key});
@@ -21,76 +22,76 @@ class ProfileItemsSection extends StatefulWidget {
 }
 
 class _ProfileItemsSectionState extends State<ProfileItemsSection> {
+  void _showLogoutDialog(BuildContext context) {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.warning,
+      animType: AnimType.scale,
+      title: 'Confirm Logout',
+      desc: 'Are you sure you want to log out?',
+      btnCancelOnPress: () {},
+      btnOkOnPress: () {
+        context.read<ProfileBloc>().add(LogOutEvent());
+      },
+      btnOkText: 'OK',
+      btnCancelText: 'Cancel',
+      btnOkColor: kAppPrimaryWrongColor,
+      btnCancelColor: Color(0xffFEB800),
+    ).show();
+  }
+
   @override
   Widget build(BuildContext context) {
     final items = ProfileConstList.profileItemModelList;
 
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        final item = items[index];
-
-        if (item.title == 'Logout') {
-          return BlocConsumer<ProfileBloc, ProfileStates>(
-            listener: (context, state) {
-              if (state is LogOutSuccess) {
-                GoRouter.of(context).go(Routes.signInViewId);
-              } else if (state is LogOutFailure) {
-                showSafeSnackBar(
-                  context,
-                  state.errorMessage,
-                  kAppPrimaryWrongColor,
-                );
-              }
-            },
-            builder: (context, state) {
-              return ProfileItem(
-                profileItemModel: item,
-                onTap: () {
-                  AwesomeDialog(
-                    context: context,
-                    dialogType: DialogType.warning,
-                    animType: AnimType.scale,
-                    title: 'Confirm Logout',
-                    desc: 'Are you sure you want to log out?',
-                    btnCancelOnPress: () {
-                      GoRouter.of(context).pop();
-                    },
-                    btnOkOnPress: () {
-                      context.read<ProfileBloc>().add(LogOutEvent());
-                    },
-                    btnOkText: 'OK',
-                    btnCancelText: 'Cancel',
-                    btnOkColor: kAppPrimaryWrongColor,
-                    btnCancelColor: kAppPrimaryBlueColor,
-                  ).show();
-                },
-              );
-            },
-          );
-        }
-
-        // باقي العناصر
-        return ProfileItem(
-          profileItemModel: item,
-          onTap: () async {
-            switch (item.title) {
-              case 'Edit Profile':
-                final result = await GoRouter.of(
-                  context,
-                ).push<bool>(item.routeName);
-                if (result == true && context.mounted) {
-                  BlocProvider.of<GetUserInfoCubit>(context).localGetUserInfo();
+    return Column(
+      children: [
+        ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return ProfileItem(
+              profileItemModel: item,
+              onTap: () async {
+                switch (item.title) {
+                  case 'Edit Profile':
+                    final result = await GoRouter.of(
+                      context,
+                    ).push<bool>(item.routeName);
+                    if (result == true && context.mounted) {
+                      BlocProvider.of<GetUserInfoCubit>(
+                        context,
+                      ).localGetUserInfo();
+                    }
+                    break;
+                  default:
+                    GoRouter.of(context).push(item.routeName);
+                    break;
                 }
-                break;
-              default:
-                GoRouter.of(context).push(item.routeName);
+              },
+            );
+          },
+        ),
+        //logout item
+        BlocConsumer<ProfileBloc, ProfileStates>(
+          listener: (context, state) {
+            if (state is LogOutSuccess) {
+              GoRouter.of(context).go(Routes.signInViewId);
+            } else if (state is LogOutFailure) {
+              showSafeSnackBar(
+                context,
+                state.errorMessage,
+                kAppPrimaryWrongColor,
+              );
             }
           },
-        );
-      },
+          builder: (context, state) {
+            return LogoutItem(onTap: () => _showLogoutDialog(context));
+          },
+        ),
+      ],
     );
   }
 }
